0327a375b44c8f840ea5f845ea6f0d990d928eaf
RickySandi <rickysandis@gmail.com>
2021-09-03 22:05:50 -0400

    data harcodeada

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Profile from './components/Profile';
 import Home from './components/Home';
 
-
 const App = () => {
 
   return (
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,10 @@ import { Button } from '@material-ui/core';
 import './Profile.css';
 
 
+var volunteer = {
+    name: 'Juan',
+    age: 25
+}
 const Profile = () => {
     const location = useLocation()
     return (
@@ -20,15 +24,16 @@ const Profile = () => {
 
                         </div>
                         <div class="name">
-                            <h2>Nombre completo</h2>
+                            <h2>{volunteer.name}</h2>
                             <p>Pequeña descripción de la persona</p>
                         </div>
-                        <Button color="primary">Datos personales</Button>
-                        <Button color="primary">Eventos asistidos</Button>
+                        <Button color="primary" borderRadius="20%">Datos personales</Button>
+                        <Button color="primary" borderRadius="20%">Eventos asistidos</Button>
                         <div class="container">
                             <div class="btn-container">
 
-
+                                <p><strong>Nombre </strong>{volunteer.name}</p>
+                                <p><strong>Edad </strong>{volunteer.age}</p>
                             </div>
                             <div class="gen-info-container">
                                 <p><strong>Proyectos en los que participa:</strong></p>
