72093c7aa5cfb1309f891995b68eb83fcbdd564a
Andrew <andrew.j.t.p@gmail.com>
2021-09-06 21:16:21 -0400

    Modal de formulario extesno funcionando y recuperando datos basicos

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,87 @@
+import React from 'react';
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import { Button } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal } from '@material-ui/core';
+import FormEditUser from "./FormEditUser"
 import './Profile.css';
 
-
-var volunteer = {
-    name: 'Juan',
-    age: 25,
-    event: 'El Agua es Oro'
+var volunteer={
+    name: 'Juanito',
+    age: 10,
+    event: 'El Agua es bronce'
 }
+
+
+
+
 function showData() {
-    document.getElementById('data').innerHTML = `<p><strong>Nombre </strong>${volunteer.name}</p> <p><strong>Edad </strong>${volunteer.age}</p>`
+    document.getElementById('data').innerHTML = `<p><strong>Nombre </strong>${volunteer.name}</p> 
+                                                 <p><strong>Edad </strong>${volunteer.age}</p>
+                                                 `
 }
 function showEvents() {
     document.getElementById('data').innerHTML = `<p><strong>Eventos: </strong>${volunteer.event}</p>`
 }
 
+
+  
+  function getModalStyle() {
+    const top = 50 ;
+    const left = 50 ;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  
+  
+
 const Profile = (onClick) => {
-    const location = useLocation()
+
+
+    const location = useLocation();
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+    
+      
+    
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const body = (
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Editar Usuario</h2>
+            <FormEditUser volunteerst={volunteer} />
+          
+          
+        </div>
+      );
+   
     return (
-        <div>
+        <div>            
             {location.pathname === '/' && (
                 <Link to='/profile'>Perfil</Link>
             )}
@@ -34,13 +96,23 @@ const Profile = (onClick) => {
                         <div class="name">
                             <h2>{volunteer.name}</h2>
                             <p>Pequeña descripción de la persona</p>
+                            <Button type="button" onClick={handleOpen} variant="contained" color="primary" >Editar Perfil</Button>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="simple-modal-title"
+                                aria-describedby="simple-modal-description"
+                            >
+                                {body}
+                            </Modal>
+                            
                         </div>
                         <Button onClick={showData} color="primary" borderRadius="20%">Datos personales</Button>
                         <Button onClick={showEvents} color="primary" borderRadius="20%">Eventos asistidos</Button>
                         <div class="container">
                             <div class="btn-container" id="data">
 
-
+                            
                             </div>
                             <div class="gen-info-container">
                                 <p><strong>Proyectos en los que participa:</strong></p>
@@ -61,3 +133,5 @@ const Profile = (onClick) => {
 }
 
 export default Profile
+
+
