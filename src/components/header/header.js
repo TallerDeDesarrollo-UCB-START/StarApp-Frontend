25e0861b112178955820e7e58b5d3b6952fa8851
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 16:31:01 -0400

    ft-4: Modify paths to export

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {ReactComponent as Logo} from '../../assets/logo,png';
-import './header.css';
+import Logo from '../../assets/logo.png';
+import './Header.css';
 
 const Header = () =>{
     return(
         <nav>
             <div>
                 <div>
-                    <Logo></Logo>
+                    <img src={Logo} />
                 </div>
             </div>
         </nav>
     )
 }
+
+export default Header;
