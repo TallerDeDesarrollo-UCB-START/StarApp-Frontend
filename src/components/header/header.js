36cf42f22673b14f165345fcdfdabfc8e9050a57
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 17:40:02 -0400

    ft-4: Add styles to the header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,8 +5,24 @@ import './Header.css';
 const Header = () =>{
     return(
         <div class="header-division">
-            <div>
-                <img src={Logo} />
+            <div class="header-logo">
+                <div>
+                    <img src={Logo} />
+                </div>
+            </div>
+            <div class="header-menu">
+                <div class="header-menu-option">
+                    <span>Home</span>
+                </div>
+                <div class="header-menu-option">
+                    <span>Proyectos</span>
+                </div>
+                <div class="header-menu-option">
+                    <span>Eventos</span>
+                </div>
+                <div class="header-menu-option">
+                    <span>Perfil</span>
+                </div>
             </div>
         </div>
     )
