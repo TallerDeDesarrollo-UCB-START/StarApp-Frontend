79101e3838849e08c99a4158a09a80d0d30ba48a
RickySandi <rickysandis@gmail.com>
2021-09-08 08:22:28 -0400

    cambios

---
+++ src/components/VolunteersList.js
@@ -0,0 +1,28 @@
+class VolunteersList extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1> Bienvenido a Evento!</h1>
+                </div>
+                <div>
+                    <Link to="/events/event/attendance">Validar asitencia de voluntarios</Link>
+                </div>
+
+            </div>
+
+
+
+
+
+        );
+    }
+}
+
+export default VolunteersList
+
+
+
+
+
