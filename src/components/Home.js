b7e1d3d735ce4086fe816e71e70d80092104c6bc
Kevin <kevindiegofloresantezana@hotmail.com>
2021-09-08 17:37:41 -0400

    Agregar todas las secciones Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import { ThemeProvider } from "@material-ui/styles";
 import { createTheme, responsiveFontSizes } from '@material-ui/core/styles';
@@ -19,47 +20,102 @@ theme = responsiveFontSizes(theme);
 const Home = () => {
     const location = useLocation()
     return (
-        <div>
-            {location.pathname === '/' && (
+        <div>         
                 
+            <Card color="primary" className="cardHome">  
+                
+                    
+                    <CardContent>
+                        <ThemeProvider theme={theme}>
+                            <Typography color="primary" align="center" variant="h1" component="h2" gutterBottom >
+                                HAZTE STARTER
+                            </Typography>   
+                        </ThemeProvider>                            
+                        <Typography className="textHome" variant="subtitle1" color="textPrimary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+                            llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
+                        </Typography>
+                    </CardContent>
+                
+                <CardActions>
+                    <Button  variant="contained" className="button" size="large" color="primary" >
+                    START
+                    </Button>
+                </CardActions>
+                </Card>
+                <Divider variant="inset"  />
                 <Card className="cardHome">
+                    <img className="imagenSeccion1" src="https://getconnected.honeywellhome.com/en/sites/getconnected.honeywell.com.en/files/lyric_to_home.png"></img>
+                    <CardContent>
+                        <ThemeProvider theme={theme}>
+                            <Typography color="primary" className="titleSeccion" variant="h3" component="h4" gutterBottom >
+                                EVENTOS & ACTIVIDADES
+                            </Typography>   
+                        </ThemeProvider>                            
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Typography>
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                            Participa en los eventos del proyecto de tu agrado
+                        </Typography>
+                        
+                    </CardContent>
+
+                </Card>
+                <Divider variant="inset"  />
+                <Card className="cardHome">
+                    <img className="imagenSeccion2" src="https://getconnected.honeywellhome.com/en/sites/getconnected.honeywell.com.en/files/lyric_to_home.png"></img>
+                    <CardContent>
+                        <ThemeProvider theme={theme}>
+                            <Typography color="primary" className="titleSeccion" variant="h3" component="h4" gutterBottom >
+                                PROYECTOS START
+                            </Typography>   
+                        </ThemeProvider>                            
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Typography>
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                            Participa en los eventos del proyecto de tu agrado
+                        </Typography>
+                        
+                    </CardContent>
+
+                </Card>
+                <Divider variant="inset"  />
+                <Card className="cardHome">
+                    <img className="imagenSeccion1" src="https://getconnected.honeywellhome.com/en/sites/getconnected.honeywell.com.en/files/lyric_to_home.png"></img>
+                    <CardContent>
+                        <ThemeProvider theme={theme}>
+                            <Typography color="primary" className="titleSeccion" variant="h3" component="h4" gutterBottom >
+                                LOGROS & RECOMPENSAS
+                            </Typography>   
+                        </ThemeProvider>                            
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Typography>
+                        <Typography  variant="subtitle1" color="textSecundary" component="p" gutterBottom>
+                            Participa en los eventos del proyecto de tu agrado
+                        </Typography>
+                        
+                    </CardContent>
+
+                </Card>
+                
+
+
+                 
                     
-                        <CardMedia
-                            style={{maxWidth: 500}}
-                            image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Home_Icon.svg/2048px-Home_Icon.svg.png"
-                            title="Contemplative Reptile"
-                        />
-                        <CardContent>
-                            <ThemeProvider theme={theme}>
-                                <Typography className="titleSeccion" variant="h1" component="h2" gutterBottom >
-                                    HAZTE STARTER
-                                </Typography>   
-                            </ThemeProvider>
-                            
-                            <Typography className="textHome" variant="body2" color="textPrimary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarcticallllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
-                                llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'''
-                            </Typography>
-                        </CardContent>
-                   
-                    <CardActions>
-                        <Button  variant="contained" className="button" size="large" color="primary" >
-                        START
-                        </Button>
-                    </CardActions>
-                 </Card>
-                    
-            )}
+            
         </div>
 
     )
