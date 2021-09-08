0a73f4add72775b8ace176aabb246d9a1179defc
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 15:16:00 -0400

    ft-4: Add logo to the code

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ReactComponent as Logo} from '../../assets/logo,png';
+
+const Header = () =>{
+    return(
+        <nav>
+            <div>
+                <div>
+                    <Logo></Logo>
+                </div>
+            </div>
+        </nav>
+    )
+}
