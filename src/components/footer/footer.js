b2e24e7f9e52f928498d754122c05e71ae9a296b
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 21:19:25 -0400

    ft-4: Add styles and boxes for footer component

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () =>{
+    return(
+        <div class="footer-section">
+            <div class="footer-divicion">
+            <div>
+                <span>Cochabamba - Bolivia</span>
+            </div>
+            <div>
+                <span>Cochabamba - Bolivia</span>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
