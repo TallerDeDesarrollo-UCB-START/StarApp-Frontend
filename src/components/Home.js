27bc7446c274bf9bbe81dccb1740a89391816638
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 21:23:50 -0400

    ft-4: Add footer component to the home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 const Home = () => {
     const location = useLocation()
     return (
         <div>
             <Header/>
+            <Footer/>
         </div>
     )
 }
