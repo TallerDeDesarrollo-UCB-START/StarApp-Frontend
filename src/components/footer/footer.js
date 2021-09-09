4e01b50cc1245d44c3011ea4dfd02e89a004fe41
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 22:23:31 -0400

    ft-4: Update styles and icons of footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () =>{
     return(
         <footer class="footer-section">
             <div class="footer-divicion">
-            <div>
-                <span>Cochabamba - Bolivia</span>
+            <div class="footer-section-option footer-text">
+                <div>
+                    <span>Cochabamba - Bolivia</span>
+                </div>
+                <div>
+                    <span>Av. America entre pando y potosi</span>
+                </div>
+                <div>
+                    <span>HORARIO</span>
+                </div>
             </div>
-            <div>
-                <span>Cochabamba - Bolivia</span>
+            <div class="footer-section-option-center">
+                <div>
+                    <span>START AMERICAS TOGETHER </span>
+                </div>
+            </div>
+            <div class="footer-section-option-icon icons">
+                <FontAwesomeIcon icon={faFacebook}/>
+            </div>
+            <div class="footer-section-option-icon icons">
+                <FontAwesomeIcon icon={faTwitter}/>
+            </div>
+            <div class="footer-section-option-icon icons">
+                <FontAwesomeIcon icon={faInstagram}/>
             </div>
             </div>
         </footer>
