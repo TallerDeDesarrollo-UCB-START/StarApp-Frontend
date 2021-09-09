d51a0a9ae96ad050ecdf4bbfcd4b880d1ce1db49
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-08 21:54:56 -0400

    ft-4: Transfer components to the app

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Profile from './components/Profile';
 import Home from './components/Home';
+import Header from './components/Header/Header';
+import Footer from './components//Footer/Footer';
+
 
 const App = () => {
 
   return (
-    <Router>
-
-      {/* <div className="App"> */}
-      <Profile />
-      <Home />
-      {/* </div> */}
-    </Router >
+    <div>
+      <div>
+        <Router>
+        <Header/>
+        {/* <div className="App"> */}
+        <Profile />
+        <Home />
+        {/* </div> */}
+        </Router >
+      </div>
+      <Footer/>
+    </div>
+    
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,10 @@
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
 
 const Home = () => {
     const location = useLocation()
     return (
         <div>
-            <Header/>
-            <Footer/>
         </div>
     )
 }
