505d4f7d32beeead03e0df1ccf1ceac5f4623344
MakJhonny <makykamacho@gmail.com>
2021-09-20 01:46:52 -0400

    boton login implementado

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import Logo from '../../assets/logo.png';
 import './header.css';
@@ -6,10 +6,11 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import { useHistory } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import {faFacebook, faTwitter, faInstagram} from '@fortawesome/free-brands-svg-icons'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme)=> ({
     root: {
       width: "100%",
       background: "none",
@@ -20,9 +21,19 @@ const useStyles = makeStyles({
     navButton: {
         borderBottom: "none"
     },
-  });
+    containerLogo: {
+        width:"80%",
+        display: "flex",
+        justifyContent:"center"
+    },
+    loginButton: {
+        width:"10%", 
+        marginRight:"30px",
+    },
+  }));
 
 const Header = () =>{
+    const [logged, setLogged] = React.useState(false)
     const routes = {
         "/": 0,
         "/projects": 1,
@@ -32,14 +43,27 @@ const Header = () =>{
     const keysRoutes = Object.keys(routes)
     let location = useLocation()
     const history = useHistory()
-    const classes = useStyles();
-    const [value, setValue] = React.useState(routes[location.pathname]);
+    const classes = useStyles()
+    const [value, setValue] = React.useState(routes[location.pathname])
+    useEffect(() => {
+        if(!Boolean(sessionStorage.getItem("jwt")) && location.pathname !== "/"){
+            history.push("/login")
+        }
+        setLogged(Boolean(sessionStorage.getItem("jwt")))
+      },[history, location.pathname]);
     return(
         <header className="header-division">
             <div className="header-logo">
-                <div>
+                <div style={{width:"10%"}}></div>
+                <div className={classes.containerLogo}>
                     <img src={Logo} alt=" "/>
                 </div>
+                <Button 
+                    className={classes.loginButton} 
+                    variant={(logged)?"filled": "outlined"}
+                    onClick={()=>history.push("/login")}>
+                        {(logged)?"Log out": "Login"}
+                </Button>
             </div>
             <div className="header-menu">
             <BottomNavigation
