3875f1334b700606baf1b53bb0583bf48f10898f
Jorge Zientarski <j.a.zientar@gmail.com>
2021-09-22 03:41:03 -0400

    Correcciones de UI

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -2,73 +2,70 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Table, Container, Card } from "reactstrap";
+import { Container, Card } from "reactstrap";
 
 const api = axios.create({
   baseURL: `http://localhost:5000/eventos`,
+  //baseURL: `https://5fc44b7b36bc7900163436cf.mockapi.io/api/Message/Eventos`
 });
 
 class EventsList extends Component {
   state = {
     events: [],
-  };
+  }
+
   constructor() {
     super();
-    api.get("/").then((res) => {
-      console.log(res.data);
-      this.setState({ events: res.data });
-    });
+    this.getEvents();
+  }
+
+  getEvents = async () => {
+    try {
+      let data = await api.get('/').then(({ data }) => data);
+      this.setState({ events: data })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
+
     return (
       <div>
         <div>
-          <h1> Bienvenido a Lista de eventos!</h1>
+          <div>
+            <h1> Bienvenido a Lista de eventos!</h1>
+          </div>
+          <div>
+            <Link to="/events/crearevento">Crear Evento</Link>
+          </div>
         </div>
-
-        <div>
-          <Link to="/events/crearevento">Crear Evento</Link>
-        </div>
-
-        <div>
-          <Link to="/events/event">Ver Evento</Link>
-        </div>
-
-        <div>
-          <Container>
-            <Card>
-              {this.state.events.map((event) => (
-                <table width="60%" align="center" cellPadding="0">
-                  <tr key={event.id}>
-                    <td rowspan="5">
-                      <img
-                        height="100%"
-                        width="100%"
-                        src="http://jorge-zientarski.com/imgs/Events2.jpg"
-                      ></img>
-                    </td>
-                    <td>Nombre: {event.nombre_evento}</td>
-                  </tr>
-                  <tr>
-                    <td>Modalidad: {event.modalidad_evento}</td>
-                  </tr>
-                  <tr>
-                    <td>Fecha: {event.fecha_evento}</td>
-                  </tr>
-                  <tr>
-                    <td>Lugar: {event.lugar_evento}</td>
-                  </tr>
-                  <tr>
-                    <td>Descripcion: {event.descripcion_evento}</td>
-                  </tr>
-                </table>
-              ))}
-            </Card>
-          </Container>
-        </div>
+        <Container>
+          <Card>
+            {this.state.events.map((event) => (
+              <div class="card w-70">
+                <div class="row no-gutters">
+                  <div class="col-auto">
+                    <img src="http://jorge-zientarski.com/imgs/Events2.jpg" class="img-fluid" alt="" />
+                  </div>
+                  <div class="col">
+                    <div class="card-block px-1">
+                      <h4 class="card-title">{event.nombre_evento}</h4>
+                      <p class="card-text"><b>Description:</b> {event.descripcion_evento}</p>
+                      <p class="card-text"><b>Modalidad:</b> {event.modalidad_evento}</p>
+                      <p class="card-text"><b>Fecha:</b> {event.fecha_evento}</p>
+                      <p class="card-text"><b>Lugar:</b> {event.lugar_evento}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-footer w-100 text-muted">
+                </div>
+              </div>
+            ))}
+          </Card>
+        </Container>
       </div>
-    );
+    )
   }
 }
 export default EventsList;
