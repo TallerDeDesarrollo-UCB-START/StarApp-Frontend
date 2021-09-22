cf56430ad14192c5a6ee55a231d81155a953b8c1
RickySandi <rickysandis@gmail.com>
2021-09-22 08:29:22 -0400

    Aumentando boton Crear Evento

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card } from "reactstrap";
+import { Container, Card, Button } from "reactstrap";
 
 const api = axios.create({
   baseURL: `http://localhost:5000/eventos`,
@@ -12,7 +12,7 @@ const api = axios.create({
 class EventsList extends Component {
   state = {
     events: [],
-  }
+  };
 
   constructor() {
     super();
@@ -21,23 +21,24 @@ class EventsList extends Component {
 
   getEvents = async () => {
     try {
-      let data = await api.get('/').then(({ data }) => data);
-      this.setState({ events: data })
+      let data = await api.get("/").then(({ data }) => data);
+      this.setState({ events: data });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   render() {
-
     return (
       <div>
         <div>
           <div>
             <h1> Bienvenido a Lista de eventos!</h1>
           </div>
-          <div>
-            <Link to="/events/crearevento">Crear Evento</Link>
+          <div style={{ display: "flex" }}>
+            <Button style={{ marginLeft: "auto" }} href="/events/crearevento">
+              Crear Evento
+            </Button>
           </div>
         </div>
         <Container>
@@ -46,26 +47,37 @@ class EventsList extends Component {
               <div class="card w-70">
                 <div class="row no-gutters">
                   <div class="col-auto">
-                    <img src="http://jorge-zientarski.com/imgs/Events2.jpg" class="img-fluid" alt="" />
+                    <img
+                      src="http://jorge-zientarski.com/imgs/Events2.jpg"
+                      class="img-fluid"
+                      alt=""
+                    />
                   </div>
                   <div class="col">
                     <div class="card-block px-1">
                       <h4 class="card-title">{event.nombre_evento}</h4>
-                      <p class="card-text"><b>Description:</b> {event.descripcion_evento}</p>
-                      <p class="card-text"><b>Modalidad:</b> {event.modalidad_evento}</p>
-                      <p class="card-text"><b>Fecha:</b> {event.fecha_evento}</p>
-                      <p class="card-text"><b>Lugar:</b> {event.lugar_evento}</p>
+                      <p class="card-text">
+                        <b>Description:</b> {event.descripcion_evento}
+                      </p>
+                      <p class="card-text">
+                        <b>Modalidad:</b> {event.modalidad_evento}
+                      </p>
+                      <p class="card-text">
+                        <b>Fecha:</b> {event.fecha_evento}
+                      </p>
+                      <p class="card-text">
+                        <b>Lugar:</b> {event.lugar_evento}
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div class="card-footer w-100 text-muted">
-                </div>
+                <div class="card-footer w-100 text-muted"></div>
               </div>
             ))}
           </Card>
         </Container>
       </div>
-    )
+    );
   }
 }
 export default EventsList;
