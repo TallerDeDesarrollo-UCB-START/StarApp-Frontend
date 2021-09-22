b91186c6967150dc82035f911383680152e0b4c6
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-21 22:20:58 -0400

    ft-22-s1: Add button style

--- src/components/componentes proyectos/atomos/ParticiparEnProyectoBtn.js
+++ src/components/componentes proyectos/atomos/ParticiparEnProyectoBtn.js
@@ -6,12 +6,7 @@ import { Container } from '@material-ui/core';
 // Merce Vic
 function ContenidoProyecto() {
     return (
-        <Container>
-            {
-                // Colocar las cosas para que el boton luzca como el
-                // wireframe de figma...
-            }
-        </Container>
+        <button>Participar</button>
     );
 }
 
