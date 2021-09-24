be5814a1ddb9d44363377c20a6d8c27a8ca09730
RickySandi <rickysandis@gmail.com>
2021-09-24 17:37:45 -0400

    Componente evento

---
+++ src/components/Evento.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Card, Button } from "reactstrap";
+
+const api = axios.create({
+  baseURL: `http://localhost:5000/eventos/`,
+  //baseURL: `https://5fc44b7b36bc7900163436cf.mockapi.io/api/Message/Eventos`
+});
+
+class Evento extends Component {
+  state = {
+    event: {},
+  };
+  constructor() {
+    super();
+    this.getListaParticipantes();
+  }
+
+  getListaParticipantes = async (event) => {
+    try {
+      let data = await api.get("/:{id}").then(({ data }) => data);
+      this.setState({ event: data });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}
+export default Evento;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,6 +9,7 @@ import Attendance from "../components/Attendance";
 import crearEvento from "../components/CrearEvento/crearEvento";
 import ProyectoContainer from "../components/ProyectoContainer";
 import FormularioProyecto from "../components/Formulario";
+import Evento from "../components/Evento";
 const Routes = [
   {
     path: "/login",
@@ -31,8 +32,8 @@ const Routes = [
     component: FormularioProyecto,
   },
   {
-    path: '/eventos',
-    name: 'EventsList',
+    path: "/eventos",
+    name: "EventsList",
     component: EventsList,
   },
   {
@@ -46,8 +47,8 @@ const Routes = [
     component: Profile,
   },
   {
-    path: '/eventos/event',
-    name: 'Evento',
+    path: "/eventos/event",
+    name: "Evento",
     component: Event,
   },
   {
@@ -61,9 +62,14 @@ const Routes = [
     component: Attendance,
   },
   {
-    path: '/eventos/crearevento',
-    name: 'crearEvento',
+    path: "/eventos/crearevento",
+    name: "crearEvento",
     component: crearEvento,
+  },
+  {
+    path: "/eventos/:id",
+    name: "Evento",
+    component: Evento,
   },
 ];
 export default Routes;
