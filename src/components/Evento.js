6620527c005c0915ddceb3b671ceb5305cb4f543
RickySandi <rickysandis@gmail.com>
2021-09-25 10:57:31 -0400

    Recuperando info evento

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Card, Button } from "reactstrap";
+import { useParams } from "react-router-dom";
 
 const api = axios.create({
   baseURL: `http://localhost:5000/eventos/`,
@@ -10,24 +11,65 @@ const api = axios.create({
 
 class Evento extends Component {
   state = {
-    event: {},
+    events: [],
   };
   constructor() {
     super();
-    this.getListaParticipantes();
+    // this.getId();
+    this.getEvneto(18);
   }
 
-  getListaParticipantes = async (event) => {
+  // getId() {
+  //   this.id = useParams();
+  // }
+
+  getEvneto = async (id) => {
     try {
-      let data = await api.get("/:{id}").then(({ data }) => data);
-      this.setState({ event: data });
+      let data = await api.get(`/${id}`).then(({ data }) => data);
+      this.setState({ events: data });
     } catch (err) {
       console.log(err);
     }
   };
 
   render() {
-    return <div>Evento 1</div>;
+    return (
+      <Container>
+        <Card>
+          {this.state.events.map((event) => (
+            <div class="card w-70">
+              <div class="row no-gutters">
+                <div class="col-auto">
+                  <img
+                    src="http://jorge-zientarski.com/imgs/Events2.jpg"
+                    class="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div class="col">
+                  <div class="card-block px-1">
+                    <h4 class="card-title">{event.nombre_evento}</h4>
+                    <p class="card-text">
+                      <b>Description:</b> {event.descripcion_evento}
+                    </p>
+                    <p class="card-text">
+                      <b>Modalidad:</b> {event.modalidad_evento}
+                    </p>
+                    <p class="card-text">
+                      <b>Fecha:</b> {event.fecha_evento}
+                    </p>
+                    <p class="card-text">
+                      <b>Lugar:</b> {event.lugar_evento}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="card-footer w-100 text-muted"></div>
+            </div>
+          ))}
+        </Card>
+      </Container>
+    );
   }
 }
 export default Evento;
