631dcfd5434ce506606b214de5d8b9baa74c0154
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-25 18:53:09 -0400

    Add editar button component

---
+++ src/components/componentes proyectos/atomos/EditarProyectoBtn.js
@@ -0,0 +1,14 @@
+// Componentes:
+import './EditarProyectoBtn.css';
+// Librerias-Paquetes-Estilos:
+import { Container } from '@material-ui/core';
+
+function EditarProyectoBtn() {
+    return (
+        <button className="editarButton">
+            Editar
+        </button>
+    )
+}
+
+export default EditarProyectoBtn
