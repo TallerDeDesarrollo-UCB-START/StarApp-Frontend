94ee89ad6c5693bd015a6fb508f80152a5f6138b
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-25 19:12:18 -0400

    Add buttons to editar and eliminar a proyect

--- src/components/componentes proyectos/moleculas/ContenidoProyecto.js
+++ src/components/componentes proyectos/moleculas/ContenidoProyecto.js
@@ -1,5 +1,8 @@
 // Componentes:
-import ParticiparEnProyectoBtn from '../atomos/ParticiparEnProyectoBtn'
+import ParticiparEnProyectoBtn from '../atomos/ParticiparEnProyectoBtn';
+import EditarProyectoBtn from '../atomos/EditarProyectoBtn';
+import EliminarProjectoBtn from '../atomos/EliminarProjectoBtn';
+
 // Librerias-Paquetes:
 import { Container } from '@material-ui/core';
 import './ContenidoProyecto.css';
@@ -9,14 +12,17 @@ import { Box } from '@material-ui/core';
 function ContenidoProyecto({proyecto}) {
     return (
         <Box className="content-container">
-            
-                <h3>Proyecto: {proyecto.titulo}</h3>
-                <p> Objetivo: {proyecto.objetivo}</p>
-                <p> Descripción: {proyecto.descripcion}</p>
-                <p> Lider: {proyecto.lider}</p>
-            
+               
+            <p>Proyecto: {proyecto.titulo}</p>
+            <p> Objetivo: {proyecto.objetivo}</p>
+            <p> Descripción: {proyecto.descripcion}</p>
+            <p> Lider: {proyecto.lider}</p>
+
             <div className="button-container">
+                <div className="space-button"></div>
                 <ParticiparEnProyectoBtn/>
+                <EditarProyectoBtn/>
+                <EliminarProjectoBtn/>
             </div>
         </Box>
     );
