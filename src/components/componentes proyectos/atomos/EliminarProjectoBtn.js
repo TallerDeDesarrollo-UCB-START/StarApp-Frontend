e7cba3c2395b369e23c6e0698ccb34db3b3ac459
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-25 19:07:03 -0400

    Add eliminar button component

---
+++ src/components/componentes proyectos/atomos/EliminarProjectoBtn.js
@@ -0,0 +1,14 @@
+// Componentes:
+import './EliminarProjectoBtn.css'
+// Librerias-Paquetes-Estilos:
+import { Container } from '@material-ui/core';
+
+function EliminarProjectoBtn() {
+    return (
+        <button className="eliminarButton">
+            Eliminar
+        </button>
+    )
+}
+
+export default EliminarProjectoBtn
