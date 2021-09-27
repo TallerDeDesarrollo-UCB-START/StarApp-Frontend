e459f746ab4fa5e3e3341847dc538cd49207a905
Victor Hugo Montano <victorhm2011@gmail.com>
2021-09-27 18:25:20 -0400

    Update imports of proyect normal component

--- src/components/componentes proyectos/organismos/BodyProyectosNormal.js
+++ src/components/componentes proyectos/organismos/BodyProyectosNormal.js
@@ -1,17 +1,17 @@
 // Componentes:
-import ProyectoVoluntario from '../moleculas/ProyectoVoluntario'
+import ProyectoVoluntarioNormal from '../moleculas/ProyectoVoluntarioNormal'
 import './BodyProyectos.css';
 // Librerias-Paquetes:
 import { Box } from '@material-ui/core';
 
 // diego
-function BodyProyectosNormal({proyectos, onEliminarProy}) {
+function BodyProyectosNormal({proyectos}) {
     return (
         <Box className="body-container">
             {
-                proyectos.map(proyecto => (
-                    <ProyectoVoluntario key={proyecto.id} proyecto={proyecto}/>
-                ))
+            proyectos.map(proyecto => (
+                <ProyectoVoluntarioNormal key={proyecto.id} proyecto={proyecto} onEliminarProy={onEliminarProy}/>
+            ))
             }
         </Box>
     );
