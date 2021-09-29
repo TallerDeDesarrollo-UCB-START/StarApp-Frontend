3302b3acae27d2f4a07d45bb9cbe5b40ccf70772
Kevin <kevindiegofloresantezana@hotmail.com>
2021-09-29 14:41:27 -0400

    Agregar bannerProyectoDetalle a ProyectoDetalle

--- src/components/componentes proyectos/moleculas/ProyectoDetalle.js
+++ src/components/componentes proyectos/moleculas/ProyectoDetalle.js
@@ -1,12 +1,14 @@
 // Componentes:
 // Librerias-Paquetes:
 import './ProyectoDetalle.css';
+import '../moleculas/BannerProyectoDetalle'
 import { Box } from '@material-ui/core';
 
 
 function ProyectoDetalle({proyecto}) {
     return (
         <Box className="content-container-detalle"> {/*agregar estilos y tamaño de box*/}
+            <BannerProyectoDetalle></BannerProyectoDetalle>
             {/*{proyecto.titulo}*/}
             {/*{proyecto.objetivo}*/}
             {/*{proyecto.descripcion}*/}
