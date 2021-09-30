cad9a3d779581cad0f8fd12d8a2063404634a6da
RickySandi <rickysandis@gmail.com>
2021-09-29 22:48:19 -0400

    changed urlDeploy eventslist.js

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card, Button, Modal, ModalHeader, ModalFooter } from "reactstrap";
+import {
+  Container,
+  Card,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalFooter,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
-
-const url = process.env.REACT_APP_API
-const urlDeploy=`${url}eventos`;
+const url = process.env.REACT_APP_API;
+const urlDeploy = `${url}eventos`;
 const api = axios.create({
-  baseURL: urlDeploy,//`http://localhost:5000/eventos`,
+  baseURL: urlDeploy, //`http://localhost:5000/eventos`,
   //baseURL: `https://5fc44b7b36bc7900163436cf.mockapi.io/api/Message/Eventos`
 });
 
-const urlParticipacion = "http://localhost:5000/eventos/participate_evento/";
+const urlParticipacion = `${url}eventos/participate_evento/`;
 
 class EventsList extends Component {
   state = {
@@ -28,18 +34,19 @@ class EventsList extends Component {
   oject = {};
 
   constructor() {
+    console.log("URL DEPLOY", urlDeploy);
     super();
     this.getEvents();
   }
 
-  abrirModal=()=>{
-    this.setState({abierto: !this.state.abierto});
-  }
+  abrirModal = () => {
+    this.setState({ abierto: !this.state.abierto });
+  };
 
   getEvents = async () => {
     try {
       let data = await api.get("/").then(({ data }) => data);
-      data = data.filter((event)=> (event.estado === '1'));
+      data = data.filter((event) => event.estado === "1");
       this.setState({ events: data });
     } catch (err) {
       console.log(err);
@@ -48,12 +55,12 @@ class EventsList extends Component {
 
   getEventsArchivados = async () => {
     try {
-      this.state.botonMostrar= true;
-      this.state.botonArchivar= false;
-      this.state.botonMostrarEventosNoArchivados= true;
-      this.state.botonMostrarEventosArchivados= false;
+      this.state.botonMostrar = true;
+      this.state.botonArchivar = false;
+      this.state.botonMostrarEventosNoArchivados = true;
+      this.state.botonMostrarEventosArchivados = false;
       let data = await api.get("/").then(({ data }) => data);
-      data = data.filter((event)=> (event.estado === '0'));
+      data = data.filter((event) => event.estado === "0");
       this.setState({ events: data });
     } catch (err) {
       console.log(err);
@@ -61,26 +68,26 @@ class EventsList extends Component {
   };
 
   deleteEvento = async (event) => {
-    console.log(event.id)
-    await axios.delete('http://localhost:5000/eventos/' + event.id);
+    console.log(event.id);
+    await axios.delete(urlDeploy + "/" + event.id);
     this.getEvents();
     this.abrirModal();
-  }
+  };
 
-  peticionArchivar =  async (event) => {
-    await axios.put('http://localhost:5000/eventos/archivar_evento/'+ event.id);
+  peticionArchivar = async (event) => {
+    await axios.put(urlDeploy + "/archivar_evento/" + event.id);
     this.getEventsArchivados();
     window.location.reload();
-  }
+  };
 
-  peticionMostrar =  async (event) => {
-    this.state.botonMostrar= false;
-    this.state.botonArchivar= true;
-    this.state.botonMostrarEventosNoArchivados= false;
-    this.state.botonMostrarEventosArchivados= true;
-    await axios.put('http://localhost:5000/eventos/mostrar_evento/'+ event.id);
+  peticionMostrar = async (event) => {
+    this.state.botonMostrar = false;
+    this.state.botonArchivar = true;
+    this.state.botonMostrarEventosNoArchivados = false;
+    this.state.botonMostrarEventosArchivados = true;
+    await axios.put(urlDeploy + "/mostrar_evento/" + event.id);
     this.getEvents();
-  }
+  };
   mensajeConfirmacionParticipacion(event) {
     window.alert(
       `Tu participación en el evento ${event.nombre_evento} fue registrada, te esperamos!`
@@ -104,13 +111,12 @@ class EventsList extends Component {
   };
 
   render() {
-
-    const modalStyles={
+    const modalStyles = {
       position: "absolute",
       top: "50%",
       left: "50%",
-      transform: 'translate(-50%,-50%)'
-    }
+      transform: "translate(-50%,-50%)",
+    };
 
     return (
       <div>
@@ -119,9 +125,30 @@ class EventsList extends Component {
             <h1> Bienvenido a Lista de eventos!</h1>
           </div>
           <div style={{ display: "flex" }}>
-            <Button style={{ marginLeft: "auto" }} href="/eventos/crearevento"> Crear Evento </Button> 
-            <Button style={{ display: this.state.botonMostrarEventosArchivados ? "block" : "none" }} onClick={()=>this.getEventsArchivados()}>Eventos Archivados</Button>
-            <Button style={{ display: this.state.botonMostrarEventosNoArchivados ? "block" : "none" }} href="/eventos">Volver</Button>
+            <Button style={{ marginLeft: "auto" }} href="/eventos/crearevento">
+              {" "}
+              Crear Evento{" "}
+            </Button>
+            <Button
+              style={{
+                display: this.state.botonMostrarEventosArchivados
+                  ? "block"
+                  : "none",
+              }}
+              onClick={() => this.getEventsArchivados()}
+            >
+              Eventos Archivados
+            </Button>
+            <Button
+              style={{
+                display: this.state.botonMostrarEventosNoArchivados
+                  ? "block"
+                  : "none",
+              }}
+              href="/eventos"
+            >
+              Volver
+            </Button>
           </div>
         </div>
         <Container>
@@ -156,23 +183,47 @@ class EventsList extends Component {
                       </Button>
                     </div>
                   </div>
-                  
+
                   <div className="principal">
                     <div className="secundario">
-                      <Button style={{ display: this.state.botonMostrar ? "block" : "none" }} onClick={()=> this.peticionMostrar(event)}>Mostrar</Button>
-                      <Button style={{ display: this.state.botonArchivar ? "block" : "none" }} onClick={()=> this.peticionArchivar(event)}>Archivar</Button>
-                      <Button color="success" onClick={this.abrirModal}>Eliminar</Button>
-                      </div ></div>
+                      <Button
+                        style={{
+                          display: this.state.botonMostrar ? "block" : "none",
+                        }}
+                        onClick={() => this.peticionMostrar(event)}
+                      >
+                        Mostrar
+                      </Button>
+                      <Button
+                        style={{
+                          display: this.state.botonArchivar ? "block" : "none",
+                        }}
+                        onClick={() => this.peticionArchivar(event)}
+                      >
+                        Archivar
+                      </Button>
+                      <Button color="success" onClick={this.abrirModal}>
+                        Eliminar
+                      </Button>
+                    </div>
+                  </div>
 
-                      <Modal isOpen={this.state.abierto} style={modalStyles}>
-                        <ModalHeader>
-                          Esta seguro de eliminar este evento ??                    
-                        </ModalHeader>
-                        <ModalFooter>
-                          <Button color="primary" onClick={() => this.deleteEvento(event)}>Aceptar</Button>
-                          <Button color="secondary" onClick={this.abrirModal}>Cancelar</Button>
-                        </ModalFooter>
-                      </Modal>
+                  <Modal isOpen={this.state.abierto} style={modalStyles}>
+                    <ModalHeader>
+                      Esta seguro de eliminar este evento ??
+                    </ModalHeader>
+                    <ModalFooter>
+                      <Button
+                        color="primary"
+                        onClick={() => this.deleteEvento(event)}
+                      >
+                        Aceptar
+                      </Button>
+                      <Button color="secondary" onClick={this.abrirModal}>
+                        Cancelar
+                      </Button>
+                    </ModalFooter>
+                  </Modal>
                 </div>
                 <div className="card-footer w-100 text-muted"></div>
               </div>
