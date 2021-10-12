5776918dd5213d92941f844f554d8c2b9677692f
dasdo <pamela.navia@salamancasolutions.com>
2021-10-12 16:29:20 -0400

    verProyectoDetalle

--- src/components/componentes proyectos/atomos/VerProyectoBtn.js
+++ src/components/componentes proyectos/atomos/VerProyectoBtn.js
@@ -4,10 +4,18 @@ import './VerProyectoBtn.css';
 
 function VerProyectoBtn({onActivarForm}) {
     return (
+<<<<<<< HEAD
         <button className="createButton" onClick={onActivarForm}>
+=======
+        <button className="viewButton" onClick={onActivarForm}>
+>>>>>>> 526e645 (verProyectoDetalle)
             Ver proyecto
         </button>
     )
 }
 
+<<<<<<< HEAD
 export default VerProyectoBtn
+=======
+export default VerProyectoBtn
+>>>>>>> 526e645 (verProyectoDetalle)
--- src/components/componentes proyectos/moleculas/ProyectoDetalle.js
+++ src/components/componentes proyectos/moleculas/ProyectoDetalle.js
@@ -14,4 +14,5 @@ function ProyectoDetalle({proyecto}) {
     );
 }
 
+
 export default ProyectoDetalle
