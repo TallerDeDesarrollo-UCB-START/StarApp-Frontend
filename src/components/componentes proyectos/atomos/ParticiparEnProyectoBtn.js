f045dc7c07bd10d5ae9fb19ad74ae7819023eef8
Victor Hugo Montano <victorhm2011@gmail.com>
2021-10-13 16:23:31 -0400

    Update method to verify volunteer participation

--- src/components/componentes proyectos/atomos/ParticiparEnProyectoBtn.js
+++ src/components/componentes proyectos/atomos/ParticiparEnProyectoBtn.js
@@ -12,45 +12,29 @@ import { makeStyles, withStyles} from "@material-ui/core/styles";
 function ParticiparEnProyectoBtn( {proyecto,  onPartiparProy, onGetParticipacion}) {
 
     // States
-
     //const [event, verificarParticipacion] = useState(0);
-
     useEffect(function () {
-        if(proyecto.id!== null) {
-            /*
-            document.getElementById("buttonCrearProyecto").classList.add('button-hide');
-            console.log('hola');*/
-            console.log(document.querySelectorAll(".participarButton"));
-            //document.querySelector(".participarButton").classList.add('button-hide');
-            //document.querySelectorAll(".participarButton").forEach(element => element.classList.add('button-hide'));
-            document.getElementById(proyecto.id).classList.add('button-hide');
-            
-        }
-      })
-    //const [titulo, setTitulo] = useState('')
-    function agregarRequerido(element){
-        element.classList.add('button-hide');
-    }
-
-    /*
-    function verificarParticipacion(element){
-        if(onGetParticipacion) {
-            element.classList.add('button-hide');
-        }
-    }*/
+        onGetParticipacion(proyecto.id).then(state => 
+            {if(state) {
+                document.getElementById(proyecto.id).classList.add('button-hide');
+            }}
+        );
+    })
 
     const onClick = (event) => {
         onPartiparProy(proyecto.id); 
+        document.getElementById(proyecto.id).classList.add('button-hide');
         alert('Registered participation'); 
-        agregarRequerido(event.currentTarget);
-        //verificarParticipacion(event.currentTarget);
     }
+
     return (
-        <ParticipateButton id={proyecto.id} variant="contained" color="secondary"
+        <div id={proyecto.id}>
+            <ParticipateButton variant="contained" color="secondary"
             onClick={onClick}
-        >
-        Participar
-        </ParticipateButton>
+            >
+                Participar
+            </ParticipateButton>
+        </div> 
     );
 }
 
--- src/components/componentes proyectos/paginas/VistaProyectos.js
+++ src/components/componentes proyectos/paginas/VistaProyectos.js
@@ -40,7 +40,7 @@ function VistaProyectos() {
     const participarEnProyecto = async (id) => { 
         const idSesion = sessionStorage.getItem("id");
         await fetch(
-        `${URLParticiparProy}/${id}/session/${idSesion}`,
+        `${URLParticiparProy}/${id}/sesion/${idSesion}`,
         { 
             method: 'PUT'
         })
@@ -55,13 +55,12 @@ function VistaProyectos() {
 
     const obtenerParticipacionProyecto = async (idProyecto) => {
         const idSesion = sessionStorage.getItem("id");
-        var response = idProyecto%2 ? true : false;
-        /*
-        const response = await fetch(`${URLParticpaVoluntario}/${idProyecto}/session/${idSesion}`,
+        const response = await fetch(`${URLParticpaVoluntario}/${idProyecto}/sesion/${idSesion}`,
         { 
             method: 'GET'
-        });*/
-        return response;
+        });
+        const data = await response.json();
+        return data;
     }
 
     const editarProyecto = async (proyectoEditar) => {
@@ -88,8 +87,6 @@ function VistaProyectos() {
         setProyectos(proyectos.filter((proy) => proy.id !== id));
     }
     
-    
-
     const rol = 'admin'
     const componenteProyectos = rol=='admin' ? <ProyectosAdmins proyectos={proyectos} onCrearProy={crearProyecto} 
         onEliminarProy={eliminarProyecto} onPartiparProy={participarEnProyecto} onEditarProy={editarProyecto} 
@@ -109,6 +106,6 @@ const URLProyecto = `${url}get_proyecto`//'http://localhost:5000/get_proyecto'//
 const URLCrearProy = `${url}create_proyecto`//'http://localhost:5000/create_proyecto'//`${url}create_proyecto`
 const URLEditarProy = `${url}update_proyecto`//'http://localhost:5000/update_proyecto'//`${url}edit_proyecto`
 const URLEliminarProy = `${url}delete_proyecto`//'http://localhost:5000/delete_proyecto'//`${url}delete_proyecto`
-//const URLParticpaVoluntario = `${url}participar_url`//'http://localhost:5000/participar_url'//`${url}participar_url`
+const URLParticpaVoluntario = `${url}participate`//'http://localhost:5000/participate'//`${url}participate`
 
 export default VistaProyectos
