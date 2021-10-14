97515271a47264a1e15f62f33275bab1855c8929
RickySandi <rickysandis@gmail.com>
2021-10-14 16:34:40 -0400

    recupernado array categorias

--- src/components/FiltroEventos.js
+++ src/components/FiltroEventos.js
@@ -1,14 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import _ from "lodash";
+import { render } from "react-dom";
+
+const api = axios.create({
+  baseURL: `http://localhost:5000/categorias`,
+  //baseURL: `https://5fc44b7b36bc7900163436cf.mockapi.io/api/Message/Eventos`
+});
 
 const FiltroEventos = (props) => {
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
+    getCategorias();
   };
+
+  const [categorias, setCategorias] = useState({
+    categorias: [],
+  });
+  const getCategorias = async () => {
+    console.log("entra");
+    let data = await api.get("/").then(({ data }) => data);
+    console.log("TYPEOF DATA", typeof data);
+    setCategorias({ categorias: data });
+    console.log("CATEGORIAS", categorias);
+  };
+
   return (
     <div className="">
       <div className="">
         <label>Categoría</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
+          {/* {this.categorias.map(({ interes }, index) => (
+            <option value={interes}>{interes}</option>
+          ))} */}
           <option value="Todas">Todas</option>
           <option value="Entretenimiento">Entretenimiento</option>
         </select>
