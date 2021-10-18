2e07ee9ab845fc1c4289a19099c18adfc495633f
Victor Hugo Montano <victorhm2011@gmail.com>
2021-10-18 18:10:54 -0400

    Add list of proyects on perfil view

--- src/components/perfil/proyectos/listaProyectos.js
+++ src/components/perfil/proyectos/listaProyectos.js
@@ -24,24 +24,11 @@ const useStyles = makeStyles((theme)=>({
     }
 }))
 
-const obtenerParticipacionProyecto = async () => {
-    const idSesion = sessionStorage.getItem("id");
-    const response = await fetch(
-      `${baseURL}/${idSesion}/get_my_proyectos`,
-      {
-        method: "GET",
-      }
-    );
-    const data = await response.json();
-    console.log(data);
-    return data;
-  };
-
 const columns = [
     {
         field: 'titulo',
         headerName: 'Titulo',
-        width: 200,
+        width: 270,
     },
     {
         field: 'lider',
@@ -62,37 +49,30 @@ const columns = [
     {
         field: 'fecha_inicio',
         headerName: 'Fecha de Inicio',
-        width: 270,
-        },
+        width: 260,
+    },
 ];
 
 const url = process.env.REACT_APP_API;
-const idSesion = sessionStorage.getItem("id");
 const baseURL=`${url}sesion`;
-//const baseURL=`${url}sesion/${idSesion}/get_my_proyectos`;
-//const baseURL=`${url}extended_form`
-//app.get("/sesion/:id_autenticacion/get_my_proyectos",async(req,res)=>
 
 function ListaProyectos () {
     const classes = useStyles()
     const [data, setData] = useState([])
     const [originalData, setOriginalData] = useState([])
     const smallScreen = useMediaQuery('(min-width:700px)')
-    const usersGet = async() => (
-        await axios.get(baseURL)
-        .then( response => {
-            var resp = response.data
-            var ids = 0
-            resp = resp.map((proyecto) => {
-                ids = ids + 1
-                return {...proyecto, id:ids}
-            })
-            setData(resp);
-            setOriginalData(resp);
-        }).catch( error => {
-            console.log(error)
-        })
-    )
+    const obtenerParticipacionProyecto = async () => {
+        const idSesion = sessionStorage.getItem("id");
+        const response = await fetch(
+          `${baseURL}/${idSesion}/get_my_proyectos`,
+          {
+            method: "GET",
+          }
+        );
+        const data = await response.json();
+        setData(data);
+        setOriginalData(data);
+    }
 
     useEffect( () => {
         obtenerParticipacionProyecto();
@@ -101,7 +81,7 @@ function ListaProyectos () {
     return (
         <section className={classes.section}>
             <div className={(smallScreen)?classes.containerSearchField: classes.smallContainerSearchField}>
-                
+               
             </div>
             <div className={classes.containerTable}>
                 <DataGrid 
