58907648d2a00db505e2c049cd84b8413eb47af6
dasdo <pamela.navia@ucb.edu.bo>
2021-10-19 18:33:59 -0400

    estilosBotonesProyectos

--- src/components/componentes proyectos/moleculas/ContenidoProyectoDetalle.js
+++ src/components/componentes proyectos/moleculas/ContenidoProyectoDetalle.js
@@ -3,19 +3,34 @@ import './ContenidoProyecto.css';
 import { Box } from '@material-ui/core';
 
 function ContenidoProyecto({proyecto}) {
-    const fechaFin = proyecto.fecha_fin?proyecto.fechaFin: "En Progreso"
+    const fechaFin = proyecto.fecha_fin?proyecto.fecha_fin: "En Progreso"
+
     return (
         <Box className="content-container">
-            
-            <p>Proyecto: {proyecto.titulo}</p>
-            <p> Objetivo: {proyecto.objetivo}</p>
-            <p> Descripción: {proyecto.descripcion}</p>
-            <p> Lider: {proyecto.lider}</p>
-            <p> Categoría: {proyecto.categoria}</p>
-            <p> Fecha de Inicio: {proyecto.fecha_inicio}</p>
-            <p> Fecha de Fin: {fechaFin}</p>
+            <b>
+                <h4 className="card-title">{proyecto.titulo}</h4>
+            </b>
+            <p className="card-text">
+                <b>Objetivo:</b> {proyecto.objetivo}
+            </p>
+            <p className="card-text">
+                <b>Descripción:</b> {proyecto.descripcion}
+            </p>
+            <p className="card-text">
+                <b>Lider:</b> {proyecto.lider}
+            </p>
+            <p className="card-text">
+                <b>Categoría:</b> {proyecto.categoria}
+            </p>
+            <p className="card-text">
+                <b>Fecha de Inicio:</b> {proyecto.fecha_inicio}
+            </p>
+            <p className="card-text">
+                <b>Fecha de Fin:</b> {fechaFin}
+            </p>
         </Box>
     );
+    
 }
  
 export default ContenidoProyecto
