cb19a15d8c3d8e6fb15e2e3d9d4838cab32c20dd
Kevin <kevindiegofloresantezana@hotmail.com>
2021-10-20 15:35:14 -0400

    componente

--- src/components/componentes proyectos/moleculas/ListaParticipantesProyecto.js
+++ src/components/componentes proyectos/moleculas/ListaParticipantesProyecto.js
@@ -1,6 +1,47 @@
-function ListaParticipantesProyecto({proyectoId}) {
-    
+// Componentes:
+// Librerias-Paquetes:
+import { Box ,Typography,List, ListItem,ListItemIcon,ListItemText} from '@material-ui/core';
+import { useState,useEffect } from 'react';
+
+function ListaParticipantesProyecto({proyectoId}){
+
+    const[participantes, setParticipantes]=useState([])
+
+    useEffect(() => {
+        const getParticipantes = async () => {
+        const participantesProyecto =  await fetchParticipantes()
+        setParticipantes(participantesProyecto)
+        }
+        getParticipantes()
+    })
+
+    // HTTP requests & functions
+    async function fetchParticipantes() {
+        const response = await fetch(`${process.env.REACT_APP_API}get_participantes_proyecto_simple/${proyectoId}`)
+        const data = await response.json()
+        return data;
+    }
     return (
-        <div></div>
+        <Box>
+            <p>Participantes:</p>
+            <List>
+            {
+                participantes.map(participante=>(
+                    <ListItem>
+                        <ListItemIcon>
+
+                        </ListItemIcon>
+                        <ListItemText primary={participante.nombre} />
+                    </ListItem>
+
+                ))
+            }
+
+            </List>
+
+
+        </Box>
+
     );
 }
+export default ListaParticipantesProyecto 
