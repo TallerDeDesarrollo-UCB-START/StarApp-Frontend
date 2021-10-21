8006fda8176683a9f6ac633fbdb4d65b784eb75b
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-10-20 22:25:51 -0400

    Warning corregido 2

--- src/components/componentes proyectos/moleculas/FormularioCrearProyecto.js
+++ src/components/componentes proyectos/moleculas/FormularioCrearProyecto.js
@@ -4,7 +4,6 @@ import InputTexto from '../moleculas/InputTexto'
 import '../moleculas/FormularioCrearProyecto.css'
 import { useState } from "react"
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 
 
