46f8aa3666b44acf94771a1b341a6bbc8ca9c6ba
Kevin <kevindiegofloresantezana@hotmail.com>
2021-10-31 03:10:34 -0400

    boton volver

--- src/components/componentes proyectos/paginas/VistaProyectoIndividual.js
+++ src/components/componentes proyectos/paginas/VistaProyectoIndividual.js
@@ -1,5 +1,6 @@
 // Componentes:
 import BodyProyectoIndividual from '../organismos/BodyProyectoIndividual'
+import VolverProyectoBtn from '../atomos/VolverProyectoBtn';
 // Librerias-Paquetes:
 import {useState, useEffect} from 'react'
 import { Box } from '@material-ui/core';
@@ -35,6 +36,7 @@ function VistaProyectoIndividual() {
     return (
         
         <Box className="body-container">
+            <VolverProyectoBtn/>
             {
                <BodyProyectoIndividual proyecto={proyecto}/>
             }
