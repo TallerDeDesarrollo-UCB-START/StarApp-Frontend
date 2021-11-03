aa194fb9559f7d2e64fc0c34c75a9868986b39fb
Kevin <kevindiegofloresantezana@hotmail.com>
2021-11-03 16:14:05 -0400

    Lista participantes fin

--- src/components/componentes proyectos/moleculas/ListaParticipantesProyecto.js
+++ src/components/componentes proyectos/moleculas/ListaParticipantesProyecto.js
@@ -1,7 +1,7 @@
 // Componentes:
 // Librerias-Paquetes:
 import { Box ,Typography,List, ListItem,ListItemIcon,ListItemText} from '@material-ui/core';
-import { useState,useEffect } from 'react';
+/*import { useState,useEffect } from 'react';
 
 function ListaParticipantesProyecto({proyectoId}){
 
@@ -44,4 +44,58 @@ function ListaParticipantesProyecto({proyectoId}){
 
     );
 }
-export default ListaParticipantesProyecto 
+export default ListaParticipantesProyecto */
+import axios from 'axios'
+import React,{Component} from 'react'
+
+export class ListaParticipantesProyecto extends Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            posts: []
+        }
+    }
+    
+    componentDidMount(){
+        let thisUrl = window.location.href;
+        let id = this.getId(thisUrl);
+        axios.get(`${process.env.REACT_APP_API}get_participantes_proyecto_simple/${id}`)
+        .then(response => {
+            console.log(response)
+            this.setState({posts:response.data})
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    getId(thisUrl) {
+        var id = thisUrl.substring(thisUrl.indexOf("/") + 1);
+        id = thisUrl.split("/").pop();
+        return id;
+      }
+
+    render(){
+        const {posts} = this.state
+        return(
+            <Box>
+                <p>Lista de Participantes:</p>
+                <List>
+                {
+                    posts.map(post=>(
+                        <ListItem>
+                            <ListItemIcon>
+
+                            </ListItemIcon>
+                            <ListItemText primary={post.nombre} />
+                        </ListItem>
+
+                    ))
+                }
+                </List>
+            </Box>
+
+        )
+    }
+}
+export default ListaParticipantesProyecto
