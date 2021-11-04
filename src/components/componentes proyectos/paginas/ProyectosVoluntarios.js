cd68f4cc3e5781b11a07e75e6454c4e4e46b4162
Victor Hugo Montano <victorhm2011@gmail.com>
2021-11-03 21:42:33 -0400

    Fix conflicts

--- src/components/componentes proyectos/paginas/ProyectosVoluntarios.js
+++ src/components/componentes proyectos/paginas/ProyectosVoluntarios.js
@@ -3,13 +3,28 @@ import HeaderProyectos from '../organismos/HeaderProyectos'
 import BodyProyectos from '../organismos/BodyProyectos'
 // Librerias-Paquetes:
 import { Box } from '@material-ui/core';
+import SelectFiltroCategoria from '../atomos/SelectFiltroCategoria';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Banner from '../../../assets/generic_picture.png';
 
 // DIEGO
 //victor y Merce
-function ProyectosVoluntarios({proyectos, rol, onPartiparProy, onGetParticipacion, onCancelarParticipacion, onNumeroParticipantes, tituloHeader}) {
+function ProyectosVoluntarios({proyectos, rol, onPartiparProy, onGetParticipacion, onCancelarParticipacion, onFiltroProy, onNumeroParticipantes}) {
+    const classes = useStyles();
+
     return (
         <Box style={styles}>
-            <HeaderProyectos tituloHeader={tituloHeader}/>
+            <HeaderProyectos />
+            
+            <SelectFiltroCategoria onFiltroProy={onFiltroProy}/>
             <BodyProyectos rol = {rol}
                             proyectos={proyectos}
                             onPartiparProy={onPartiparProy}
@@ -24,5 +39,11 @@ const styles= {
     minHeight: "650px"
     //border: "4px solid orange"
 }
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+  });
 
 export default ProyectosVoluntarios
