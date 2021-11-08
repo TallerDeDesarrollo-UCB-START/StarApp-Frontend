356e95a048bb3135db6b8d2c82ea8f95fa5d3ad6
julietaArgollas <julieta.c.argollas@gmail.com>
2021-11-08 15:38:46 -0400

    vista para un evento

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -184,48 +184,50 @@ class Evento extends Component {
     return (
       <Container>
         <Card>
-          {this.state.events.map((event) => (
-            <div className="card w-70" key={event.id}>
+        {this.state.events.map((event) => (
+            <div key={event.id}>
               <div className="row no-gutters">
                 <div className="col-auto">
                   <img
                     src="http://jorge-zientarski.com/imgs/Events2.jpg"
                     className="img-fluid"
                     alt=""
+                    align="center"
                   />
                 </div>
 
                 <div className="col">
                   <div className="row">
-                    <h4 className="card-title">{event.nombre_evento}</h4>
+                    <h4 className="card-title">
+                      <b>{event.nombre_evento}</b>
+                    </h4>
                   </div>
 
                   <div className="row">
                     <div className="col">
                       <div className="card-block px-1">
                         <p className="card-text">
-                          <b>Descripción:</b> {event.descripcion_evento}
-                        </p>
-                        <p className="card-text">
-                          <b>Categoría:</b> {event.categoria}
-                        </p>
-                        <p className="card-text">
-                          <b>Modalidad:</b> {event.modalidad_evento}
+                          <b>Hora Inicio:</b> {event.hora_inicio}
                         </p>
                         <p className="card-text">
                           <b>Fecha:</b> {event.fecha_evento}
+                        </p>
+                        <p className="card-text">
+                          <b>Proyecto:</b> {event.proyecto}
+                        </p>
+
+                        <p className="card-text">
+                          <b>Modalidad:</b> {event.modalidad_evento}
+                        </p>
+
+                        <p className="card-text">
+                          <b>Descripción:</b> {event.descripcion_evento}
                         </p>
                       </div>
                     </div>
 
                     <div className="col">
                       <div className="card-block px-1">
-                        <p className="card-text">
-                          <b>Lider:</b> {event.lider}
-                        </p>
-                        <p className="card-text">
-                          <b>Hora Inicio:</b> {event.hora_inicio}
-                        </p>
                         <p className="card-text">
                           <b>Hora Fin:</b> {event.hora_fin}
                         </p>
@@ -233,47 +235,44 @@ class Evento extends Component {
                           <b>Lugar:</b> {event.lugar_evento}
                         </p>
                         <p className="card-text">
-                          <b>Proyecto:</b> {event.proyecto}
+                          <b>Categoría:</b> {event.categoria}
+                        </p>
+                        <p className="card-text">
+                          <b>Lider:</b> {event.lider}
                         </p>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="card-footer w-100 text-muted"></div>
             </div>
           ))}
-
-          <Button
-            className="botonEditarEvento"
-            variant="contained"
-            color="primary"
-            size="medium"
-            onClick={() => this.abrilModalEditarEvento()}
-          >
-            Editar
-          </Button>
+          <br></br>
         </Card>
-
-        <h1 className="TituloListaParticipantes">Lista de participantes</h1>
-        <Card>
+        <br></br>
+        <Button
+          className="botonEditarEvento"
+          variant="contained"
+          color="primary"
+          size="small"
+          onClick={() => this.abrilModalEditarEvento()}
+        >
+          Editar
+        </Button>
+        <br></br>
+        <br></br>
+        <div className="listForm">
+          <h4 className="card-list-1">
+            <b>Lista De Participantes</b>
+          </h4>
           {this.state.participants.map((participant) => (
-            <div className="card w-70" key={participant.id}>
-              <div className="row no-gutters">
-                <div className="col">
-                  <div className="card-block px-1">
-                    <p className="card-text"></p>
-                    <p className="card-text">
-                      <b> Nombre:</b> {participant.nombre}{" "}
-                      {participant.apellido}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="card-footer w-100 text-muted"></div>
+            <div className="Participante" key={participant.id}>
+              <p className="card-list">
+                <b> Nombre:</b> {participant.nombre} {participant.apellido}
+              </p>
             </div>
           ))}
-        </Card>
+        </div>
 
         <Modal
           id="ModalFormEditEvento"
