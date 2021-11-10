90d0eff7f72866f43c3f0a717e10e3da6e075f43
alvarofloresf <45455677+alvarofloresf@users.noreply.github.com>
2021-11-10 12:04:15 -0400

    vista general eventos

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card, Modal, Button } from "reactstrap";
+import { Container, Card, Modal } from "reactstrap";
 import { Link } from "react-router-dom";
 import ModalHeader from "react-bootstrap/ModalHeader";
 import ModalFooter from "react-bootstrap/ModalFooter";
+import { Box, Button, Grid } from "@material-ui/core";
 
 import "./EventsList.css";
 
@@ -325,10 +326,12 @@ class EventsList extends Component {
               })}
             </select>
           </div>
-          <div style={{ display: "flex" }}>
+          <Box style={{ display: "flex" }}>
             {rolUser !== "voluntario" ? (
               <Fragment>
                 <Button
+                  variant="contained"
+                  color="secondary"
                   style={{ marginLeft: "auto" }}
                   onClick={() => this.mostrarModalInsertar()}
                 >
@@ -372,103 +375,164 @@ class EventsList extends Component {
             >
               Volver
             </Button>
-          </div>
+          </Box>
         </div>
         <Container>
           <Card>
-            {this.state.events.map((event) => (
-              <div className="card w-70" key={event.id}>
-                <div className="row no-gutters">
-                  <div className="col-auto">
-                    <img
-                      src="http://jorge-zientarski.com/imgs/Events2.jpg"
-                      className="img-fluid"
-                      alt=""
-                    />
+            <div class="container">
+              {this.state.events.map((event) => (
+                <div class="card-container" key={event.id}>
+                  <div class="header">
+                    <a href="#">
+                      <img
+                        src="http://jorge-zientarski.com/imgs/Events2.jpg"
+                        alt=""
+                      />
+                    </a>
                   </div>
-                  <div className="col">
-                    <div className="card-block px-1">
-                      <h4 className="card-title">{event.nombre_evento}</h4>
-                      <p className="card-text">
-                        <b>Descripción:</b> {event.descripcion_evento}
-                      </p>
-                      <p className="card-text">
-                        <b>Modalidad:</b> {event.modalidad_evento}
-                      </p>
-                      <p className="card-text">
-                        <b>Fecha:</b> {event.fecha_evento}
-                      </p>
-                      <p className="card-text">
-                        <b>Lugar:</b> {event.lugar_evento}
-                      </p>
-
-                      <p className="card-text">
-                        <b>Categoría:</b> {event.categoria}
-                      </p>
-
-                      {this.validarBotones(event) ? (
-                        <Button
-                          onClick={() => {
-                            this.postParticipacion(event);
-                          }}
-                        >
-                          {" "}
-                          Participar
-                        </Button>
+                  <div class="descripcion">
+                    <p>
+                      <b>{event.nombre_evento}</b>
+                    </p>
+                    <p className="card-text">
+                      <b>Fecha:</b> {event.fecha_evento}
+                    </p>
+                    <p className="card-text">
+                      <b>Hora:</b> {event.hora_inicio}
+                    </p>
+                    <p>{event.descripcion_evento}</p>
+                    <p className="card-text">
+                      <b>Lugar:</b> {event.lugar_evento}
+                    </p>
+                    <p className="card-text">
+                      <b>Modalidad:</b> {event.modalidad_evento}
+                    </p>
+                    <p className="card-text">
+                      <b>Lider:</b> {event.lider}
+                    </p>
+                    <p className="card-text">
+                      <b>Categoria:</b> {event.categoria}
+                    </p>
+                    <div className="buttons">
+                      {rolUser !== "voluntario" ? (
+                        <Grid container spacing={6}>
+                          {this.validarBotones(event) ? (
+                            <Grid item xs>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={() => {
+                                  this.postParticipacion(event);
+                                }}
+                              >
+                                {" "}
+                                Participar
+                              </Button>
+                            </Grid>
+                          ) : (
+                            <Grid item xs="auto">
+                              <Button
+                                variant="contained"
+                                onClick={() => {
+                                  this.eliminarParticipacion(event);
+                                }}
+                              >
+                                {" "}
+                                Eliminar Participacion
+                              </Button>
+                            </Grid>
+                          )}
+                          <Grid item xs>
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              style={{
+                                display: this.state.botonMostrar
+                                  ? "block"
+                                  : "none",
+                              }}
+                              onClick={() => this.peticionMostrar(event)}
+                            >
+                              Mostrar
+                            </Button>
+                          </Grid>
+                          <Grid item xs>
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              style={{
+                                display: this.state.botonArchivar
+                                  ? "block"
+                                  : "none",
+                              }}
+                              onClick={() => this.peticionArchivar(event)}
+                            >
+                              Archivar
+                            </Button>
+                          </Grid>
+                          <Grid item xs>
+                            <Button
+                              variant="contained"
+                              color="success"
+                              onClick={() => this.deleteEvento(event)}
+                            >
+                              Eliminar
+                            </Button>
+                          </Grid>
+                          <Grid item xs="auto">
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              justify="align"
+                            >
+                              <Link to={"eventos/" + event.id}>Ver Evento</Link>
+                            </Button>
+                          </Grid>
+                        </Grid>
                       ) : (
-                        <Button
-                          onClick={() => {
-                            this.eliminarParticipacion(event);
-                          }}
-                        >
-                          {" "}
-                          Eliminar Participacion
-                        </Button>
+                        <Grid container alignItems="flex-start" justify="flex-end" direction="row">
+                          {this.validarBotones(event) ? (
+                            <Grid item xs>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={() => {
+                                  this.postParticipacion(event);
+                                }}
+                              >
+                                {" "}
+                                Participar
+                              </Button>
+                            </Grid>
+                          ) : (
+                            <Grid item xs="auto">
+                              <Button
+                                variant="contained"
+                                onClick={() => {
+                                  this.eliminarParticipacion(event);
+                                }}
+                              >
+                                {" "}
+                                Eliminar Participacion
+                              </Button>
+                            </Grid>
+                          )}
+                          <Grid item xs="auto">
+                            <Button variant="contained" color="primary">
+                              <Link to={"eventos/" + event.id}>Ver Evento</Link>
+                            </Button>
+                          </Grid>
+                        </Grid>
                       )}
-
-                      <Button>
-                        <Link to={"eventos/" + event.id}>Ver Evento</Link>
-                      </Button>
                     </div>
                   </div>
-                  {rolUser !== "voluntario" ? (
-                    <Fragment>
-                      <div className="principal">
-                        <div className="secundario">
-                          <Button
-                            style={{
-                              display: this.state.botonMostrar
-                                ? "block"
-                                : "none",
-                            }}
-                            onClick={() => this.peticionMostrar(event)}
-                          >
-                            Mostrar
-                          </Button>
-                          <Button
-                            style={{
-                              display: this.state.botonArchivar
-                                ? "block"
-                                : "none",
-                            }}
-                            onClick={() => this.peticionArchivar(event)}
-                          >
-                            Archivar
-                          </Button>
+                </div>
+              ))}
+            </div>
 
-                          <Button
-                            color="success"
-                            onClick={() => this.deleteEvento(event)}
-                          >
-                            Eliminar
-                          </Button>
-                        </div>
-                      </div>
-                    </Fragment>
-                  ) : (
-                    <></>
-                  )}
-
+            {this.state.events.map((event) => (
+              <div key={event.id}>
+                <div>
                   <Modal isOpen={this.state.abierto} style={modalStyles}>
                     <ModalHeader>
                       Esta seguro de eliminar este evento ??
@@ -486,7 +550,6 @@ class EventsList extends Component {
                     </ModalFooter>
                   </Modal>
                 </div>
-                <div className="card-footer w-100 text-muted"></div>
               </div>
             ))}
           </Card>
