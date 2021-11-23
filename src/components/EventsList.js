86fbe6d2c6467bb8bf94219ee91ee661dd94afe6
alvarofloresf <45455677+alvarofloresf@users.noreply.github.com>
2021-11-23 16:15:37 -0400

    fix text2

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -551,7 +551,7 @@ class EventsList extends Component {
                     </DialogTitle>
                     <DialogContent>
                       <DialogContentText id="alert-dialog-slide-description">
-                        ¿Está seguro que desea eliminar éste evento {event.nombre_evento} ?
+                        ¿Está seguro que desea eliminar el evento {event.nombre_evento} ?
                       </DialogContentText>
                       <DialogContentText id="alert-dialog-slide-description">
                         Se eliminara definitivamente.
