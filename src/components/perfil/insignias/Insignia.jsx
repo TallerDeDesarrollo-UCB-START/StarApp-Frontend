881b3333d2c072b01e8f71c01936e43ae917cbc8
Antonio Medina <toomedinaamp2010@gmail.com>
2021-11-24 03:00:07 -0400

    Se avanzo en las insignias

--- src/components/perfil/insignias/Insignia.jsx
+++ src/components/perfil/insignias/Insignia.jsx
@@ -1,13 +1,47 @@
 import { Typography } from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
+import SchoolIcon from '@material-ui/icons/School';
+import Grid from "@material-ui/core/Grid";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(4),
+    height: 140,
+    width: 250,
+  },
+}));
 
-function Insignia(props) {
-    return (
-        <Typography>
-            {props.insignia}
-        </Typography>
-    );
+function Insignia({ insignias, insigniasOfUser }) {
+  const classes = useStyles();
+
+  return (
+    <>
+      {insignias.map((insignia, id) => {
+        if (insigniasOfUser.insignias.includes(insignia.insignia)) {
+          return (
+            <Grid key={id} item>
+              <Paper className={classes.paper}>
+                <SchoolIcon />
+                <Typography color="primary">{insignia.insignia}</Typography>
+              </Paper>
+            </Grid>
+          );
+        } else {
+          return (
+            <Grid key={id} item>
+              <Paper className={classes.paper}>
+                <EmojiEventsIcon />
+                <Typography>{insignia.insignia}</Typography>
+              </Paper>
+            </Grid>
+          );
+        }
+      })}
+    </>
+  );
 }
 
 export default Insignia;
--- src/components/perfil/insignias/listaInsignias.jsx
+++ src/components/perfil/insignias/listaInsignias.jsx
@@ -1,40 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles} from '@material-ui/core';
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import { useMediaQuery, Typography } from "@material-ui/core";
 import axios from "axios";
-import Paper from "@mui/material/Paper";
 import Insignia from "./Insignia";
-import { styled } from "@mui/material/styles";
 
-const useStyles = makeStyles((theme) => ({
-  section: {
-    width: "100%",
+const useStyles = makeStyles((theme)=>({
+  styleGrid: {
+     textAlign: 'center',
   },
-  containerSearchField: {
-    display: "flex",
-    width: "100%",
-    justifyContent: "space-between",
+  root: {
+    flexGrow: 1,
   },
-  smallContainerSearchField: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "column",
-  },
-  containerTable: {
-    height: 400,
-    width: "100%",
-  },
-}));
-
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  borderRadius: "30000",
-  color: theme.palette.text.secondary,
-}));
+}))
 
 const url = process.env.REACT_APP_API;
 const baseURL = `${url}insignias`;
@@ -42,25 +19,19 @@ const api = axios.create({
   baseURL: baseURL,
 });
 function ListaInsignias() {
-  const classes = useStyles();
-  const [data, setData] = useState([]);
-  const [dataUsuario, setDataUsuario] = useState([]);
-  //const [originalData, setOriginalData] = useState([])
-  const smallScreen = useMediaQuery("(min-width:700px)");
+  const classes = useStyles()
+  const [insignias, setInsignias] = useState([]);
+  const [insigniasOfUser, setInsigniasUser] = useState([]);
   const obtenerInsigniasUsuario = async () => {
     const idSesion = sessionStorage.getItem("id");
     let dataUsuario = await api
       .get(`${baseURL}/${idSesion}`)
       .then((dataUsuario) => dataUsuario);
-    setDataUsuario(dataUsuario);
-    console.log(data);
-    //console.log(dataUsuario.data);
+    setInsigniasUser(dataUsuario.data.data);
   };
   const obtenerInsignias = async () => {
     let dataLocal = await api.get(`${baseURL}`).then((data) => data);
-    console.log(dataLocal.data);
-    setData(dataLocal);
-    // console.log(data.data);
+    setInsignias(dataLocal.data.data);
   };
 
   useEffect(() => {
@@ -69,10 +40,9 @@ function ListaInsignias() {
   }, []);
 
   return (
-    <Box>
-      {/* <Grid>{data.data.map((insignia) => insignia.insignia)} */}
-      <Grid></Grid>
-    </Box>
+    <Grid className={classes.styleGrid} justifyContent="center" container spacing={3}>
+      <Insignia insignias={insignias} insigniasOfUser={insigniasOfUser} />
+    </Grid>
   );
 }
 
