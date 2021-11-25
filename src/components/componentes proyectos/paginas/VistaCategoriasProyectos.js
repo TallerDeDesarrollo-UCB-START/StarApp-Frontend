414fd8959bf20ccd9be43b1d2e719355a35226a6
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-11-25 19:20:03 -0400

    Crear desde vista categorias funcionando

--- src/components/componentes proyectos/paginas/VistaCategoriasProyectos.js
+++ src/components/componentes proyectos/paginas/VistaCategoriasProyectos.js
@@ -33,17 +33,13 @@ function VistaCategoriasProyectos() {
 
     // Endpoint fetch
     const crearProyecto = async (nuevoProyecto) => {
-        /*const response = await fetch(
+        const response = await fetch(
             URLCrearProy,
             {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify(nuevoProyecto)
             })
-        const data = await response.json()
-        setProyectos([...proyectos, data])
-        setActualizar(!actualizar)*/
-    
     }
 
     // Methods:
@@ -65,25 +61,6 @@ function VistaCategoriasProyectos() {
 
 const url = process.env.REACT_APP_API;
 const URLCategorias = `${url}get_categoria_proyectos`//``http://localhost:5000/get_categorias`//`
+const URLCrearProy = `${url}create_proyecto`//'http://localhost:5000/create_proyecto'//
 
 export default VistaCategoriasProyectos;
-
-/*
-const categorias2 = [
-    {
-        id: 1,
-        tipo: 'ambiental',
-        imagen: 'https://media.istockphoto.com/photos/mount-hood-oregon-picture-id1268487061?b=1&k=20&m=1268487061&s=170667a&w=0&h=3fHYwaImlqUETcjCnSV7YO2-PzCFvaX6VSQaiGfWqpc='
-    },
-    {
-        id: 2,
-        tipo: 'social',
-        imagen: 'https://media.istockphoto.com/photos/life-is-amazing-when-you-have-the-greatest-friends-around-picture-id648817898?b=1&k=20&m=648817898&s=170667a&w=0&h=9OGTLvfpfy4Ce4BM__-yDb9RlErBGJRMv7irZMhFLfY='
-    },
-    {
-        id: 3,
-        tipo: 'animalista',
-        imagen: 'https://media.istockphoto.com/photos/female-veterinarian-holding-a-little-dog-in-her-arms-picture-id1280869192?b=1&k=20&m=1280869192&s=170667a&w=0&h=Fhsw2VrPIhhmKz1gSAjqS7lH_s0KNUSVO9-Obi7CFJE='
-    },
-]
-*/
