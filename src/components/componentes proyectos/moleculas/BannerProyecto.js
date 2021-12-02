a2204b8f95582dc156697f6aaedffe8b141ee0df
Victor Hugo Montano <victorhm2011@gmail.com>
2021-12-01 20:03:35 -0400

    Delete unsed vars

--- src/components/componentes proyectos/moleculas/BannerProyecto.js
+++ src/components/componentes proyectos/moleculas/BannerProyecto.js
@@ -1,7 +1,6 @@
 // Componentes:
 
 // Librerias-Paquetes:
-import Banner from '../../../assets/example.png';
 import { makeStyles } from '@material-ui/core/styles';
 import CardMedia from '@material-ui/core/CardMedia';
 import './BannerProyecto.css';
