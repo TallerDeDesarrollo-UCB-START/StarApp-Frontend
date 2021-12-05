11cfe608108ebfd1c4cfdb998c55753b1c7050ce
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-12-05 00:52:35 -0400

    Formulario crear corregido

--- src/components/componentes proyectos/moleculas/FormularioCrearProyecto.js
+++ src/components/componentes proyectos/moleculas/FormularioCrearProyecto.js
@@ -9,9 +9,11 @@ import { Button, Modal} from '@material-ui/core';
 //import { Button, Modal, FormData, FormControl, MenuItem, Select, InputLabel} from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useForm, /*SubmitHandler,*/ FormProvider } from "react-hook-form";
 
 
 function FormularioCrearProyecto({ onCrearProy, onActivarForm, mostrarFormCrear}) {
+    const methods = useForm()
     // States
     const [fechaInicio, setFechaInicio] = useState('')
     const [fechaFin, setFechaFin] = useState('')
@@ -61,8 +63,8 @@ function FormularioCrearProyecto({ onCrearProy, onActivarForm, mostrarFormCrear}
         return true
     }
 
-    const onSubmit = (event) => {
-        event.preventDefault() // To avoid submitting to an actual page
+    const onSubmit = (data) => {
+        /*event.preventDefault() // To avoid submitting to an actual page
         const lideres = [lider]
         const objetivos = [objetivo]
         if (validarCampos(event) === false) {
@@ -80,8 +82,9 @@ function FormularioCrearProyecto({ onCrearProy, onActivarForm, mostrarFormCrear}
             informacion_adicional: informacion_adicional,
             //image: image,
             url_imagen: url_imagen
-        }
-        onCrearProy(nuevoProyecto) // callback invocation
+        }*/
+debugger
+        onCrearProy(data) // callback invocation
         resetStates()
         onActivarForm() // Oculta el formulario
     }
@@ -122,73 +125,90 @@ function FormularioCrearProyecto({ onCrearProy, onActivarForm, mostrarFormCrear}
     };
     const body = (
         <div style={modalStyle} className="paper-crear">
-            <form  onSubmit={onSubmit}  enctype="multipart/form-data">
-                {botonCancelarFormulario}
-                <div className="crear-container-title">
-                    <h4>Crear Proyecto</h4>
-                </div>
-                <div style={{padding: "1% 3% 0 5%"}}>
-                    <label>Fecha de Inicio</label>
-                    <InputTexto type="date"
-                                value={fechaInicio}
-                                onChange={onChangeFechaInicio}
-                                />
-                    <label>Fecha de Fin</label>
-                    <InputTexto type="date"
-                                value={fechaFin}
-                                onChange={onChangeFechaFin}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Nombre del proyecto'
-                                value={titulo}
-                                onChange={onChangeTitulo}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Descripción'
-                                value={descripcion}
-                                onChange={onChangeDescrip}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Objetivo'
-                                value={objetivo}
-                                onChange={onChangeObjetivo}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Líder'
-                                value={lider}
-                                onChange={onChangeLider}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Categoría'
-                                value={categoria}
-                                onChange={onChangeCategoria}
-                                />
-                    <InputTexto type="text"
-                                placeHolder='Estado'
-                                value={estado}
-                                onChange={onChangeEstado}
-                                />
-                    <InputTexto type="link"
-                                placeHolder='Información Adicional'
-                                value={informacion_adicional}
-                                onChange={onChangeInfoAd}
-                                />
-                    
-                    <label>
-                        Imagen por Link
-                    </label>
-                    <InputTexto
-                        type="text" 
-                        name="image" 
-                        value={url_imagen}
-                        onChange={onChangeImagenUrl}
-                    />
-                    <div className="btn-crear-container">
-                        <input type='submit' value='CREAR PROYECTO' className='btn-proy-crear btn-proy-block'/>
-                        
+            <FormProvider {...methods}>
+                <form  onSubmit={methods.handleSubmit(onSubmit)}  enctype="multipart/form-data">
+                    {botonCancelarFormulario}
+                    <div className="crear-container-title">
+                        <h4>Crear Proyecto</h4>
                     </div>
-                </div>
-            </form>
+                    <div style={{padding: "1% 3% 0 5%"}}>
+                        <InputTexto type="date"
+                                    tituloLabel={"Fecha de Fin"}
+                                    nameId="fechaInicio"
+                                    value={fechaInicio}
+                                    onChange={onChangeFechaInicio}
+                                    />
+                        <InputTexto type="date"
+                                    tituloLabel={"Fecha de Inicio"}
+                                    nameId="fechaFin"
+                                    value={fechaFin}
+                                    onChange={onChangeFechaFin}
+                                    />
+                        <InputTexto type="text"
+                                    tituloLabel={"Nombre del Proyecto"}
+                                    placeHolder='Nombre del proyecto'
+                                    nameId="titulo"
+                                    value={titulo}
+                                    onChange={onChangeTitulo}
+                                    />
+                        <InputTexto type="text"
+                                    placeHolder='Descripción'
+                                    tituloLabel={"Descripción"}
+                                    nameId="descripcion"
+                                    value={descripcion}
+                                    onChange={onChangeDescrip}
+                                    />
+                        <InputTexto type="text"
+                                    tituloLabel={"Objetivo"}
+                                    nameId="objetivo"
+                                    placeHolder='Objetivo'
+                                    value={objetivo}
+                                    onChange={onChangeObjetivo}
+                                    />
+                        <InputTexto type="text"
+                                    nameId="lider"
+                                    placeHolder='Líder'
+                                    value={lider}
+                                    onChange={onChangeLider}
+                                    />
+                        <InputTexto type="text"
+                                    nameId="categoria"
+                                    placeHolder='Categoría'
+                                    value={categoria}
+                                    onChange={onChangeCategoria}
+                                    />
+                        <InputTexto type="text"
+                                    nameId="estado"
+                                    placeHolder='Estado'
+                                    value={estado}
+                                    onChange={onChangeEstado}
+                                    />
+                        <InputTexto type="link"
+                                        tituloLabel="Información Adicional"
+                                        nameId="informacion_adicional"
+                                    placeHolder='Información Adicional'
+                                    value={informacion_adicional}
+                                    onChange={onChangeInfoAd}
+                                    />
+                        
+                        <label>
+                            Imagen por Link
+                        </label>
+                        <InputTexto
+                            type="text" 
+                            tituloLabel="Imagen por Link"
+                            nameId="image_url"
+                            name="image" 
+                            value={url_imagen}
+                            onChange={onChangeImagenUrl}
+                        />
+                        <div className="btn-crear-container">
+                            <input type='submit' value='CREAR PROYECTO' className='btn-proy-crear btn-proy-block'/>
+                            
+                        </div>
+                    </div>
+                </form>
+            </FormProvider>
             
         </div>);
     
