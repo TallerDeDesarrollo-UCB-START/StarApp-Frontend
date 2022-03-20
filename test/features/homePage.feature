e9c7415812c1e63fad573d3a82f7f75766a5910d
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2022-03-20 02:32:38 -0400

    First somke test and passing

---
+++ test/features/homePage.feature
@@ -0,0 +1,7 @@
+Feature: Visited the dev american together
+
+    Example 
+    Scenario: Visited american together
+    Given I´ve visited the page "https://dev-front-startamericas.web.app/"
+    When I´ve tried to into the web page
+    Then Show me home page
---
+++ test/features/steps_definitions/homePage.js
@@ -0,0 +1,29 @@
+const { Given, When, Then, AfterAll } = require('@cucumber/cucumber');
+const { Builder, By, Capabilities, Key } = require('selenium-webdriver');
+const { expect } = require('chai');
+
+require("chromedriver");
+
+// driver setup
+const capabilities = Capabilities.chrome();
+capabilities.set('chromeOptions', { "w3c": false });
+const driver = new Builder().withCapabilities(capabilities).build();
+let BaseUrl="";
+Given('I´ve visited the page {string}',async function (url) {
+    // Write code here that turns the phrase above into concrete actions
+    BaseUrl=url;
+});
+When('I´ve tried to into the web page',{timeout: 60 * 1000},async function () {
+    // Write code here that turns the phrase above into concrete actions
+    await driver.get(BaseUrl);
+    await driver.manage().window().maximize();
+});
+Then('Show me home page',async function () {
+    let xpath = '//*[@id="root"]/div[2]/div[1]/section/div[1]/div/div[2]/button[1]/span[1]';
+    let AuxText= await driver.findElement(By.xpath(xpath)).getText().then(r=>r);
+    expect(AuxText).to.be.equal("ÚNETE A START")
+});
+AfterAll(async () => {
+    await driver.close();
+  });
+ 
