df399fdfbac92c220560e7ff453da6d2961dd7f5
CC <73244043+Carlos9855@users.noreply.github.com>
2022-04-18 23:55:27 -0400

    add valit

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -899,6 +899,7 @@ class EventsList extends Component {
             <TextField
               className="HoraInicioEventoCrear textInput"
               name="hora_inicio"
+              //type="time"
               type="time"
               onChange={this.handleChange}
             />
