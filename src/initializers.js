6e07d97a2b412f0a30d3c91058eec60ab6c2bb42
Jacovzap <jacovzap@gmail.com>
2022-05-11 00:47:15 -0400

    s1

--- src/initializers.js
+++ src/initializers.js
@@ -1,6 +1,5 @@
 import fb from "firebase";
 
-/*
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,15 +10,15 @@ const config = {
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 fb.initializeApp(config);
-*/
-const config = {
-  apiKey: "AIzaSyDCikfkwlg_z6rYjtd5kq0wRMWx2tVqd_Y",
-  authDomain: "dev-front-startamericas.firebaseapp.com",
-  projectId: "dev-front-startamericas",
-  storageBucket: "dev-front-startamericas.appspot.com",
-  messagingSenderId: "306336007020",
-  appId: "1:306336007020:web:39613791a44b34ca80a949",
-  measurementId: "G-RMBTG4Y43Z",
-};
-fb.initializeApp(config);
-export default fb;
+
+// const config = {
+//   apiKey: "AIzaSyDCikfkwlg_z6rYjtd5kq0wRMWx2tVqd_Y",
+//   authDomain: "dev-front-startamericas.firebaseapp.com",
+//   projectId: "dev-front-startamericas",
+//   storageBucket: "dev-front-startamericas.appspot.com",
+//   messagingSenderId: "306336007020",
+//   appId: "1:306336007020:web:39613791a44b34ca80a949",
+//   measurementId: "G-RMBTG4Y43Z",
+// };
+// fb.initializeApp(config);
+// export default fb;
