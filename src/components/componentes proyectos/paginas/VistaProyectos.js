5e82d66b88de70413ac047674e59e23e605a7839
AlvaroCori <bacs200018@gmail.com>
2022-05-12 03:19:41 -0400

    exceptions ready

--- src/components/componentes proyectos/paginas/VistaProyectos.js
+++ src/components/componentes proyectos/paginas/VistaProyectos.js
@@ -6,42 +6,21 @@ import PuertaPermisos from '../organismos/PuertaPermisos';
 import {SCOPES} from '../organismos/map-permisos';
 // Librerias-Paquetes:
 import {useState, useEffect, useRef} from 'react'
-import {useLocation, useHistory} from "react-router-dom";
-import redirectErrorPage from "../../../components/redirect status/RedirectErrorPage";
-import React from "react";
-import BadRequests from '../../redirect status/BadRequests';
-import SnackbarMessage from "../../../components/templates/SnackbarMessage";
+import {useLocation} from "react-router-dom";
+
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 
 function VistaProyectos() {
     // ==== HOOKS ==== 
-    const history = useHistory();
     const [proyectos, setProyectos] = useState([])
     const [lideres, setLideres] = useState([])
     const [categorias, setCategorias] = useState([])
     const [proyectosPasadosCategoria, setProyectosPasadosCategoria] = useState([])
     const [actualizar, setActualizar] = useState(false)
     const mountedRef = useRef(false) // Bandera para saber si un componente esta desmontado o no, para evitar warning de cancelacion abrubta de llamadas asincronas
-    const [snackbar, setSnackbar] = React.useState({
-        message: "",
-        active: false,
-        severity: "success",
-        afterClose: () => {},
-      });  
-    const activeSnackbar = (message, severity, afterClose) => {
-        setSnackbar({ message, severity, afterClose, active: true });
-      };
 
-    const launchAlert = (problemMessage) => {
-        let message = BadRequests(500);
-        activeSnackbar(
-          problemMessage+message,
-          "error",
-          () => {}
-        );  
-    };
     // ==== VARIABLES ====
     let categoria = useQuery().get("categoria");
     let complementoHeader = categoria//categoria? categoria : tipoEstado
@@ -57,48 +36,24 @@ function VistaProyectos() {
     useEffect(() => {
         mountedRef.current = true
         const getProyectos = async () => {
-            try{
-                const proyectosDelServer =  await fetchProyectos()
-                setProyectosCheck(proyectosDelServer, mountedRef.current)
-            } catch (error) {
-                console.log(error);
-                //launchAlert("No se pudo obtener los proyectos, ");
-            }
+            const proyectosDelServer =  await fetchProyectos()
+            setProyectosCheck(proyectosDelServer, mountedRef.current)
         }
         const getProyectosPorCategoria = async () => {
-            try{
-                const proyectosFiltrados = await fetchProyectosPorCategoria(categoria)
-                setProyectosCheck(proyectosFiltrados, mountedRef.current)
-            } catch (error) {
-                launchAlert("No se pudo obtener los proyectos por categoria, ");
-            }
+            const proyectosFiltrados = await fetchProyectosPorCategoria(categoria)
+            setProyectosCheck(proyectosFiltrados, mountedRef.current)
         }
         const getProyectosPasadosPorCategoria = async () => {
-            try{
-                const proyectosPasados =  await fetchProyectosPasadosPorCategoria(categoria)
-                setProyectosCheck(proyectosPasados, mountedRef.current, true)
-            } catch (error) {
-                console.log(error);
-                //launchAlert("No se pudo obtener los proyectos pasados por categoria, ");
-            }
+            const proyectosPasados =  await fetchProyectosPasadosPorCategoria(categoria)
+            setProyectosCheck(proyectosPasados, mountedRef.current, true)
         }
         const getLideres = async ()=>{
-            try{
-                const lideresDelServer = await fetchLideres()
-                mountedRef.current && setLideres(lideresDelServer)
-            } catch (error) {
-                console.log(error);
-                //launchAlert("No se pudo obtener la lista de lideres o roles, ");
-            }
+            const lideresDelServer = await fetchLideres()
+            mountedRef.current && setLideres(lideresDelServer)
         }
         const getCategorias = async () => {
-            try{
-                const categosServer = await fetchCategorias()
-                mountedRef.current && setCategorias(categosServer)
-            } catch (error) {
-                console.log(error);
-                //launchAlert("No se pudo obtener las categorias, ");
-            }
+            const categosServer = await fetchCategorias()
+            mountedRef.current && setCategorias(categosServer)
         }
         
 
@@ -116,95 +71,60 @@ function VistaProyectos() {
     // ==== HTTP REQUESTS & FUNCIONES ====
     // GETs
     async function fetchProyectos() {
-        try{
-            const response = await fetch(URLProyectos)
-            const data = await response.json()
-            return data;
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener los proyectos, ");
-        }
+        const response = await fetch(URLProyectos)
+        const data = await response.json()
+        return data;
     }
 
     const fetchProyectosPorCategoria = async(categoria) => {
-        try{
-            const response= await fetch(`${URLProyectos}/${categoria}`)
-            const data = response && response.status === 204? [] : await response.json(); //FIXME: Evita un warning, pero backend deberia hacer esta validacion del status 204 y no frontend
-            return data
-            //setProyectos(proyectos.filter((proy) => proy.categoria == categoria));
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener los proyectos por categoria, ");
-        }
+        const response= await fetch(`${URLProyectos}/${categoria}`)
+        const data = response && response.status === 204? [] : await response.json(); //FIXME: Evita un warning, pero backend deberia hacer esta validacion del status 204 y no frontend
+        return data
+        //setProyectos(proyectos.filter((proy) => proy.categoria == categoria));
     }
 
     async function fetchProyectosPasadosPorCategoria(categoria) {
-        try{
-            const response = await fetch(`${URLProyectosPasados}/${categoria}`)
-            const data = await response.json()
-            return data;
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener los proyectos por categoria, ");
-        }
+        const response = await fetch(`${URLProyectosPasados}/${categoria}`)
+        const data = await response.json()
+        return data;
     }
     
     const fetchCategorias = async () => {
-        try{
-            const response = await fetch(URLCategorias)
-            const data = await response.json()
-            return data
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener las categorias, ");
-        }
+        const response = await fetch(URLCategorias)
+        const data = await response.json()
+        return data
     }
 
     async function fetchLideres() {
-        try{
-            const response = await fetch(URLLideres)
-            const data = await response.json()
-            let dataLider=[]
-            let index=1
-            for (let x of data ) {
-                dataLider.push({"id": index,"nombre":`${x.nombre}`})
-                index++
-            }
-            //dataLider.pop()
-            return dataLider;
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener los lideres, ");
+        const response = await fetch(URLLideres)
+        const data = await response.json()
+        let dataLider=[]
+        let index=1
+        for (let x of data ) {
+            dataLider.push({"id": index,"nombre":`${x.nombre}`})
+            index++
         }
+        //dataLider.pop()
+        return dataLider;
     }
 
     const obtenerParticipacionProyecto = async (idProyecto) => {
-        try{
-            const idSesion = sessionStorage.getItem("id");
-            const response = await fetch(`${URLParticpaVoluntario}/${idProyecto}/sesion/${idSesion}`,
-            { 
-                method: 'GET'
-            });
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener las participaciones del proyecto, ");
-        }
+        const idSesion = sessionStorage.getItem("id");
+        const response = await fetch(`${URLParticpaVoluntario}/${idProyecto}/sesion/${idSesion}`,
+        { 
+            method: 'GET'
+        });
+        const data = await response.json();
+        return data;
     }
 
     const obtenerNumeroParticipantes = async (idProyecto) => {
-        try{
-            const response = await fetch(`${URLNumeroParticipantes}/${idProyecto}`,
-            { 
-                method: 'GET'
-            });
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.log(error);
-            //launchAlert("No se pudo obtener el numero de participantes, ");
-        }
+        const response = await fetch(`${URLNumeroParticipantes}/${idProyecto}`,
+        { 
+            method: 'GET'
+        });
+        const data = await response.json();
+        return data;
     }
 
     /*async function fetchProyectosPasados() {
@@ -215,91 +135,67 @@ function VistaProyectos() {
     
     // CREATEs
     const crearProyecto = async (nuevoProyecto) => {
-        try{
-            const response = await fetch(
-                URLCrearProy,
-                {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json'},
-                    body: JSON.stringify(nuevoProyecto)
-                })
-            const data = await response.json()
-            setProyectos([...proyectos, data])
-            setActualizar(!actualizar) //Para activar useEffect sin causar loop infinito
-        } catch (error) {
-            //redirectErrorPage(error.response.status,history);
-            console.log(error);
-        }
+        const response = await fetch(
+            URLCrearProy,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(nuevoProyecto)
+            })
+        const data = await response.json()
+        setProyectos([...proyectos, data])
+        setActualizar(!actualizar) //Para activar useEffect sin causar loop infinito
+    
     }
     
     // UPDATEs
     const editarProyecto = async (proyectoEditar) => {
-        try{
-            //const response = 
-            await fetch(
-                `${URLEditarProy}/${proyectoEditar.id}`,
-                {
-                    method: 'PUT',
-                    headers: { 'Content-Type': 'application/json'},
-                    body: JSON.stringify(proyectoEditar)
-                })
-            //const data = await response.json()
-            //setProyectos([...proyectos.filter((proy) => proy.id !== proyectoEditar.id), data]) actualizar proyecto manualmente
-            setActualizar(!actualizar) //Para activar useEffect sin causar loop infinito
-        } catch (error) {
-            //redirectErrorPage(error.response.status,history);
-            console.log(error);
-        }
+        //const response = 
+        await fetch(
+            `${URLEditarProy}/${proyectoEditar.id}`,
+            {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(proyectoEditar)
+            })
+        //const data = await response.json()
+        //setProyectos([...proyectos.filter((proy) => proy.id !== proyectoEditar.id), data]) actualizar proyecto manualmente
+        setActualizar(!actualizar) //Para activar useEffect sin causar loop infinito
     }
 
     const participarEnProyecto = async (id) => { 
         const idSesion = sessionStorage.getItem("id");
-        try{
-            const response = await fetch(
-            `${URLParticiparProy}/${id}/sesion/${idSesion}`,
-            { 
-                method: 'PUT'
-            })
-            const data = await response.json()
-            //setActualizar(!actualizar)
-            return data
-        } catch (error) {
-            //redirectErrorPage(error.response.status,history);
-            console.log(error);
-        }
+        const response = await fetch(
+        `${URLParticiparProy}/${id}/sesion/${idSesion}`,
+        { 
+            method: 'PUT'
+        })
+        const data = await response.json()
+        //setActualizar(!actualizar)
+        return data
     }
     
     // DELETEs
     const cancelarParticipacionProyecto = async (id) => { 
         const idSesion = sessionStorage.getItem("id");
-        try{
-            const response = await fetch(
-                `${URLCancelarParticipProy}/${id}/sesion/${idSesion}`,
-                { 
-                    method: 'DELETE'
-                })
-            const data = await response.json()
-            //setActualizar(!actualizar)
-            return data
-        } catch (error) {
-            //redirectErrorPage(error.response.status,history);
-            console.log(error);
-        }
-    }
-
-    const eliminarProyecto = async (id) => { 
-        try{
-            await fetch(
-            `${URLEliminarProy}/${id}`,
+        const response = await fetch(
+            `${URLCancelarParticipProy}/${id}/sesion/${idSesion}`,
             { 
                 method: 'DELETE'
             })
-        
-            setProyectos(proyectos.filter((proy) => proy.id !== id));
-        } catch (error) {
-            //redirectErrorPage(error.response.status,history);
-            console.log(error);
-        }
+        const data = await response.json()
+        //setActualizar(!actualizar)
+        return data
+    }
+
+    const eliminarProyecto = async (id) => { 
+        await fetch(
+        `${URLEliminarProy}/${id}`,
+        { 
+            method: 'DELETE'
+        })
+    
+        setProyectos(proyectos.filter((proy) => proy.id !== id));
     }
 
     // ==== COMPONENTES: ====
@@ -335,7 +231,6 @@ function VistaProyectos() {
             <PuertaPermisos scopes={[SCOPES.canNotCrudProyectos]}>
                 {proyectosVoluntarios}
             </PuertaPermisos>
-            <SnackbarMessage snackbar={snackbar} setActive={setSnackbar} />
         </>
     );
 }
