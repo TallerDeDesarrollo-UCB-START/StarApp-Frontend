900ed6baeed46c3167502563cf145c40131b69bf
AlvaroCori <bacs200018@gmail.com>
2022-05-24 23:29:54 -0400

    corrections

--- src/initializers.js
+++ src/initializers.js
@@ -1,4 +1,3 @@
-/*
 import fb from "firebase";
 
 const config = {
@@ -13,16 +12,3 @@ const config = {
 fb.initializeApp(config);
 export default fb;
 
-*/
-import fb from "firebase";
-const config = {
-  apiKey: "AIzaSyDCikfkwlg_z6rYjtd5kq0wRMWx2tVqd_Y",
-  authDomain: "dev-front-startamericas.firebaseapp.com",
-  projectId: "dev-front-startamericas",
-  storageBucket: "dev-front-startamericas.appspot.com",
-  messagingSenderId: "306336007020",
-  appId: "1:306336007020:web:39613791a44b34ca80a949",
-  measurementId: "G-RMBTG4Y43Z",
-};
-fb.initializeApp(config);
-export default fb;
